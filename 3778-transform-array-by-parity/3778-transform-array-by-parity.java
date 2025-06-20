3a1c7e9187c93eea9a86a6905eaf15523545f4e0
Jassi Tiwari <jassitiwari550@gmail.com>
2025-06-20 15:15:45 +0530

    Time: 1 ms (100%), Space: 44.8 MB (90.64%) - LeetHub

--- 3778-transform-array-by-parity/3778-transform-array-by-parity.java
+++ 3778-transform-array-by-parity/3778-transform-array-by-parity.java
@@ -1,14 +1,27 @@
 class Solution {
     public int[] transformArray(int[] arr) {
+        //my approach 
+
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]%2==0){
+        //         arr[i]=0;
+        //     }
+        //     else{
+        //         arr[i]=1;
+        //     }
+        // }
+        // Arrays.sort(arr);
+        // return arr;
+
+        //better approach
+        int Evencount=0;
         for(int i=0;i<arr.length;i++){
             if(arr[i]%2==0){
-                arr[i]=0;
-            }
-            else{
-                arr[i]=1;
+                Evencount++;
             }
         }
-        Arrays.sort(arr);
+        Arrays.fill(arr,0,Evencount,0);
+        Arrays.fill(arr,Evencount,arr.length,1);
         return arr;
     }
 }
