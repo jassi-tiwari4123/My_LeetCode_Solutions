977eb594e935078fc80f5efa9c4a8ec81dce1605
Jassi Tiwari <jassitiwari550@gmail.com>
2025-06-25 22:13:21 +0530

    Time: 3 ms (40.07%), Space: 41.8 MB (47.56%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if((s.charAt(i)=='(') || (s.charAt(i)=='[') || (s.charAt(i)=='{')){
+                st.push(s.charAt(i));
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                char ch=st.pop();
+                if((ch=='(' && s.charAt(i)==')') || (ch=='[' && s.charAt(i)==']') ||
+                (ch=='{' && s.charAt(i)=='}')){
+                    continue;
+                }
+                return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
