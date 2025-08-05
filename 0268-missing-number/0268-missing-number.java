36e8555310f7424d09dafc6170d1894d6ffba3de
Jassi Tiwari <jassitiwari550@gmail.com>
2025-08-05 21:07:02 +0530

    Time: 0 ms (100%), Space: 45.5 MB (30.75%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,17 +1,28 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        for(int i=0;i<=nums.length+1;i++){
-            boolean flag=false;
-            for(int j=0;j<nums.length;j++){
-                if(nums[j]==i){
-                    flag=true;
-                    break;
-                }
-            }
-            if(flag==false){
-                return i;
-            }
+        // Naive approach
+        // for(int i=0;i<=nums.length+1;i++){
+        //     boolean flag=false;
+        //     for(int j=0;j<nums.length;j++){
+        //         if(nums[j]==i){
+        //             flag=true;
+        //             break;
+        //         }
+        //     }
+        //     if(flag==false){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        // better approach is to use hash
+        // best approach is to use sum of first n numbers
+        int n=nums.length;
+        long sum1=(long)((n*(n+1))/2);
+        long sum2=0;
+        for(int i=0;i<n;i++){
+            sum2+=nums[i];
         }
-        return -1;
+        return (int)(sum1-sum2);
     }
 }
