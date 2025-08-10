d47906adb8f2de0045d7600c3bf86d452c331aa9
Jassi Tiwari <jassitiwari550@gmail.com>
2025-08-10 19:20:25 +0530

    Time: 0 ms (100%), Space: 42.2 MB (30.66%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,17 +1,31 @@
 class Solution {
     public void sortColors(int[] arr) {
-         for(int i=0;i<arr.length-1;i++){
-            for(int j=i+1;j>0;j--){
-                if(arr[j]<arr[j-1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j-1];
-                    arr[j-1]=temp;
-                }
-                else{
-                    break;
-                }
+        //Dutch national flag algorithm
+        // from 0 to low-1=all elements are 0
+        // from low to mid-1=all elements are 1
+        // from mid to high=all elements are unsorted
+        // high to n-1=all elements are 2
+        int low=0;
+        int mid=0;
+        int high=arr.length-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                swap(arr,mid,low);
+                low++;
+                mid++;
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+            else{
+                swap(arr,mid,high);
+                high--;
             }
         }
-        // Arrays.sort(arr);
+    }
+    public void swap(int[] arr,int first,int last){
+        int temp=arr[first];
+        arr[first]=arr[last];
+        arr[last]=temp;
     }
 }
