eaecdd48083486b78aa75a04a8e8653855d7c7dd
Jassi Tiwari <jassitiwari550@gmail.com>
2025-09-23 23:45:57 +0530

    Time: 0 ms (100%), Space: 44.4 MB (93.58%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,37 +11,38 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        // if(head==null || head.next==null){
-        //     return null;
-        // }
-        // ListNode slow=head;
-        // ListNode fast=head;
-        // while(fast.next!=null && fast.next.next!=null){
-        //     slow=slow.next;
-        //     fast=fast.next.next;
-        //     if(slow==fast){
-        //         ListNode n=head;
-        //         while(n!=slow){
-        //             n=n.next;
-        //             slow=slow.next;
-        //         }
-        //         return n;
-        //     }
-        // }
-        // return null;
-
-        HashSet<ListNode> seen=new HashSet<>();
         if(head==null || head.next==null){
             return null;
         }
-        ListNode x=head;
-        while(x!=null){
-            if(seen.contains(x)){
-                return x;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                ListNode n=head;
+                while(n!=slow){
+                    n=n.next;
+                    slow=slow.next;
+                }
+                return n;
             }
-            seen.add(x);
-            x=x.next;
         }
         return null;
+
+        // Not an optimised way taking o(n) space
+        // HashSet<ListNode> seen=new HashSet<>();
+        // if(head==null || head.next==null){
+        //     return null;
+        // }
+        // ListNode x=head;
+        // while(x!=null){
+        //     if(seen.contains(x)){
+        //         return x;
+        //     }
+        //     seen.add(x);
+        //     x=x.next;
+        // }
+        // return null;
     }
 }
