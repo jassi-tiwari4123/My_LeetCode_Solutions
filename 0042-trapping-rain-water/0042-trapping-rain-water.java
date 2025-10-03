07c7e462140424b54284344bf6f59f2229615ac3
Jassi Tiwari <jassitiwari550@gmail.com>
2025-10-03 17:52:00 +0530

    Time: 1 ms (60.23%), Space: 46.3 MB (68.23%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int trap(int[] height) {
+        int n=height.length;
+        int[] prefmax=new int[n];
+        int[] suffmax=new int[n];
+        prefmax[0]=height[0];
+        suffmax[n-1]=height[n-1];
+        for(int i=1;i<n;i++){
+            prefmax[i]=Math.max(prefmax[i-1],height[i]);
+        }
+        for(int i=n-2;i>=0;i--){
+            suffmax[i]=Math.max(suffmax[i+1],height[i]);
+        }
+        int total=0;
+        for(int i=0;i<n;i++){
+            int leftMax=prefmax[i];
+            int rightMax=suffmax[i];
+            if(height[i]<leftMax && height[i]<rightMax){
+                total+=Math.min(leftMax,rightMax)-height[i];
+            }
+        }
+        return total;
+    }
+}
