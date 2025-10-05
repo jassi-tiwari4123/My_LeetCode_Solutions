50657ea902390ba057d8c8d287a89e74fc0e7060
Jassi Tiwari <jassitiwari550@gmail.com>
2025-10-05 21:38:00 +0530

    Time: 6 ms (72.19%), Space: 44.9 MB (43.03%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<tokens.length;i++){
+            if(isOperator(tokens[i])){
+                int op1=st.pop();
+                int op2=st.pop();
+                int result=0;
+                if(tokens[i].equals("+")){
+                    result= op2+op1;
+                }
+                else if(tokens[i].equals("-")){
+                    result= op2-op1;
+                }
+                else if(tokens[i].equals("*")){
+                    result= op2*op1;
+                }
+                else if(tokens[i].equals("/")){
+                    result= op2/op1;
+                }
+                st.push(result);
+            }
+            else{
+                st.push(Integer.parseInt(tokens[i]));
+            }
+        }
+        return st.pop();
+    }
+    public boolean isOperator(String x){
+        return (x.equals("+") || x.equals("-") || x.equals("/") || x.equals("*"));
+    }
+}
