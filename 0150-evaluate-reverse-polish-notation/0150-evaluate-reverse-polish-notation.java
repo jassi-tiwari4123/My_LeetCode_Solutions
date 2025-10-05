b083434fdb165a9b323799323cb8d17e97098f20
Jassi Tiwari <jassitiwari550@gmail.com>
2025-10-05 21:51:29 +0530

    Time: 6 ms (72.19%), Space: 44.4 MB (96.86%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,22 +1,56 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> st=new Stack<>();
+    //     Stack<Integer> st=new Stack<>();
+    //     for(int i=0;i<tokens.length;i++){
+    //         if(isOperator(tokens[i])){
+    //             int op1=st.pop();
+    //             int op2=st.pop();
+    //             int result=0;
+    //             if(tokens[i].equals("+")){
+    //                 result= op2+op1;
+    //             }
+    //             else if(tokens[i].equals("-")){
+    //                 result= op2-op1;
+    //             }
+    //             else if(tokens[i].equals("*")){
+    //                 result= op2*op1;
+    //             }
+    //             else if(tokens[i].equals("/")){
+    //                 result= op2/op1;
+    //             }
+    //             st.push(result);
+    //         }
+    //         else{
+    //             st.push(Integer.parseInt(tokens[i]));
+    //         }
+    //     }
+    //     return st.pop();
+    // }
+    // public boolean isOperator(String x){
+    //     return (x.equals("+") || x.equals("-") || x.equals("/") || x.equals("*"));
+    // }
+
+
+    // using switch case
+    Stack<Integer> st=new Stack<>();
         for(int i=0;i<tokens.length;i++){
             if(isOperator(tokens[i])){
                 int op1=st.pop();
                 int op2=st.pop();
                 int result=0;
-                if(tokens[i].equals("+")){
-                    result= op2+op1;
-                }
-                else if(tokens[i].equals("-")){
-                    result= op2-op1;
-                }
-                else if(tokens[i].equals("*")){
-                    result= op2*op1;
-                }
-                else if(tokens[i].equals("/")){
-                    result= op2/op1;
+                switch(tokens[i]){
+                    case "+":
+                        result= op2+op1;
+                        break;
+                    case "-":
+                        result= op2-op1;
+                        break;
+                    case "*":
+                        result= op2*op1;
+                        break;
+                    case "/":
+                        result= op2/op1;
+                        break;
                 }
                 st.push(result);
             }
@@ -26,7 +60,7 @@ class Solution {
         }
         return st.pop();
     }
-    public boolean isOperator(String x){
+    public static boolean isOperator(String x){
         return (x.equals("+") || x.equals("-") || x.equals("/") || x.equals("*"));
     }
 }
