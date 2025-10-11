4e74fc63b208ec0b0cf925d3fe5d9cde88d8044b
Jassi Tiwari <jassitiwari550@gmail.com>
2025-10-11 23:56:32 +0530

    Time: 1 ms (23.05%), Space: 40.7 MB (91.2%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,9 +1,14 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
+        // if(n<=0) return false;
+        // while(n%4==0){
+        //     n=n/4;
+        // }
+        // return n==1;
+
+        //checking whether the log4(n) is an integer
         if(n<=0) return false;
-        while(n%4==0){
-            n=n/4;
-        }
-        return n==1;
+        double value=Math.log10(n)/Math.log10(4);
+        return value==Math.floor(value);
     }
 }
