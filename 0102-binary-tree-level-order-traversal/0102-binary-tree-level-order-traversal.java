f8ae4ec7b9e317b5d2b80a2b42d2e07683711222
Jassi Tiwari <jassitiwari550@gmail.com>
2025-10-12 23:19:53 +0530

    Time: 0 ms (100%), Space: 45.3 MB (13%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -15,24 +15,39 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
+        // List<List<Integer>> result=new ArrayList<>();
+        // if(root==null){
+        //     return result;
+        // }
+        
+        // Queue<TreeNode> queue=new LinkedList<>();
+        // queue.add(root);
+        // while(!queue.isEmpty()){
+        //     int size=queue.size();
+        //     List<Integer> level=new ArrayList<>();
+        //     for(int i=0;i<size;i++){
+        //         TreeNode t=queue.poll();
+        //         level.add(t.val);
+        //         if(t.left!=null) queue.add(t.left);
+        //         if(t.right!=null) queue.add(t.right);
+        //     }
+        //     result.add(level);
+        // }
+        // return result;
+
+
+        //without using queue
         List<List<Integer>> result=new ArrayList<>();
-        if(root==null){
-            return result;
-        }
-        Queue<TreeNode> queue=new LinkedList<>();
-        queue.add(root);
-        while(!queue.isEmpty()){
-            int size=queue.size();
-            List<Integer> level=new ArrayList<>();
-            for(int i=0;i<size;i++){
-                TreeNode t=queue.poll();
-                level.add(t.val);
-                if(t.left!=null) queue.add(t.left);
-                if(t.right!=null) queue.add(t.right);
-            }
-            result.add(level);
-            
-        }
+        level(root,0,result);
         return result;
+    }
+    public void level(TreeNode node,int x,List<List<Integer>> result){
+        if(node==null) return;
+        if(result.size()==x){
+            result.add(new ArrayList<>());
+        }
+        result.get(x).add(node.val);
+        level(node.left,x+1,result);
+        level(node.right,x+1,result);
     }
 }
