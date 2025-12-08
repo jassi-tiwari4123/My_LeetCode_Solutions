bf17537eddd695a8ac488e2b3989eac529d2501c
Jassi Tiwari <jassitiwari550@gmail.com>
2025-12-08 15:36:14 +0530

    Time: 13 ms (56.16%), Space: 41.8 MB (66.18%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.java
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -1,15 +1,27 @@
 class Solution {
     public int countTriples(int n) {
         int count=0;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n;j++){
+        //         for(int k=1;k<=n;k++){
+        //             if(i*i + j*j ==k*k){
+        //                 count++;
+        //             }
+        //         }
+        //     }
+        // }
+        // return count;
+
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                for(int k=1;k<=n;k++){
-                    if(i*i + j*j ==k*k){
-                        count++;
-                    }
+                int sum=i*i +j*j;
+                int k=(int)Math.sqrt(sum);
+                if(k<=n && k*k==sum){
+                    count++;
                 }
             }
         }
         return count;
     }
 }
+
