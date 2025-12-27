424086bf77e8f682447d6b9ec57224f746c235e2
Jassi Tiwari <jassitiwari550@gmail.com>
2025-12-27 17:48:23 +0530

    Time: 178 ms (57.82%), Space: 80.1 MB (31.27%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.java
+++ 1711-count-good-meals/1711-count-good-meals.java
@@ -1,23 +1,21 @@
 class Solution {
     public int countPairs(int[] arr) {
-        int MOD = 1_000_000_007;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        long res = 0;
-
-        for (int x : arr) {
-            int power = 1;
-            // Loop through powers of 2 up to 2^21
-            for (int i = 0; i <= 21; i++) {
-                int need = power - x;
-                if (map.containsKey(need)) {
-                    res = (res + map.get(need)) % MOD;
+        int n=arr.length;
+        int MOD=1000000007;
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        long res=0;
+        
+        for(int j=0;j<n;j++){
+            int pow=1;
+            for(int i=0;i<22;i++){
+                int y=pow-arr[j];
+                if(hm.containsKey(y)){
+                    res=(res+hm.get(y))%MOD;
                 }
-                power <<= 1; // equivalent to 2^i
+                pow<<=1;
             }
-            // Add current number to map
-            map.put(x, map.getOrDefault(x, 0) + 1);
+            hm.put(arr[j],hm.getOrDefault(arr[j],0)+1);
         }
-
-        return (int) res;
+        return (int)res;
     }
 }
