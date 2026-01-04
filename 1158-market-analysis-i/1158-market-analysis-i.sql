9962e15dd97d63fb48a536626e3f5bcc79b24a64
Jassi Tiwari <jassitiwari550@gmail.com>
2026-01-04 22:44:06 +0530

    Time: 1228 ms (34.5%), Space: 0B (100%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -2,4 +2,4 @@
 select u.user_id as buyer_id,u.join_date,
 count(o.order_id) as "orders_in_2019"
 from users u left join orders o on o.buyer_id=
-u.user_id and order_date>"2018-12-31" group by u.user_id,u.join_date ;
+u.user_id and order_date>"2018-12-31" group by u.user_id ;
