344bb22e8fe53cab1cbae2a1cd8c2798807e2f77
Jassi Tiwari <jassitiwari550@gmail.com>
2026-01-08 12:18:40 +0530

    Time: 0 ms (100%), Space: 42.9 MB (21.42%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int combinationSum4(int[] nums, int target){
+        int n=nums.length;
+        int[] dp=new int[target+1];
+        Arrays.fill(dp,-1);
+        return fxn(nums,target,dp);
+    }
+    public int fxn(int[] nums,int target,int[] dp){
+        if(target==0) return 1;
+        if(target<0) return 0;
+        if(dp[target]!=-1) return dp[target];
+        int res=0;
+        for(int i=0;i<nums.length;i++){
+            res+=fxn(nums,target-nums[i],dp);
+        }
+        return dp[target]=res;
+    }
+}
