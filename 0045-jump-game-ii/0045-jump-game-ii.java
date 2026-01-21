4ed4e42a39efa27dd1b24e70a38d0b67c5019988
Jassi Tiwari <jassitiwari550@gmail.com>
2026-01-21 12:55:06 +0530

    Time: 1 ms (99.52%), Space: 47.2 MB (64.49%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,21 +1,32 @@
 class Solution {
     public int jump(int[] nums) {
         //recursive code
-        int n=nums.length;
-        int[] dp=new int[n];
-        Arrays.fill(dp,-1);
-        return fxn(nums,0,dp);
-    }
-    public int fxn(int[] nums,int ind,int[]dp){
-        if(ind>=nums.length-1){
-            return 0;
+    //     int n=nums.length;
+    //     int[] dp=new int[n];
+    //     Arrays.fill(dp,-1);
+    //     return fxn(nums,0,dp);
+    // }
+    // public int fxn(int[] nums,int ind,int[]dp){
+    //     if(ind>=nums.length-1){
+    //         return 0;
+    //     }
+    //     if(dp[ind]!=-1) return dp[ind];
+    //     int min=Integer.MAX_VALUE;
+    //     for(int i=1;i<=nums[ind];i++){
+    //         int jumps=fxn(nums,ind+i,dp);
+    //         if(jumps!=Integer.MAX_VALUE) min=Math.min(min,1+jumps);
+    //     }
+    //     return dp[ind]=min;
+    // }
+
+        int cnt = 0, curr = 0, farthest = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            if (i == curr) {
+                cnt++;
+                curr = farthest;
+            }
         }
-        if(dp[ind]!=-1) return dp[ind];
-        int min=Integer.MAX_VALUE;
-        for(int i=1;i<=nums[ind];i++){
-            int jumps=fxn(nums,ind+i,dp);
-            if(jumps!=Integer.MAX_VALUE) min=Math.min(min,1+jumps);
+        return cnt;
         }
-        return dp[ind]=min;
-    }
 }
