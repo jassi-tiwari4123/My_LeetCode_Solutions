dbc5de4afccc253fa9f5cfb19318e55c2d01339a
Jassi Tiwari <jassitiwari550@gmail.com>
2026-02-03 22:40:18 +0530

    Time: 3 ms (36.34%), Space: 122.7 MB (19.72%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -9,19 +9,53 @@ class Solution {
         // return b;
 
         //adjacency list
+        // int n=edges.length+1;
+        // ArrayList<ArrayList<Integer>> res=new ArrayList<>();
+        // for(int i=0;i<=n;i++){
+        //     res.add(new ArrayList<>());
+        // }
+        // for (int[] e : edges) {
+        //     int u = e[0];
+        //     int v = e[1];
+        //     res.get(u).add(v);
+        //     res.get(v).add(u);
+        // }
+        // for(int i=1;i<=n;i++){
+        //     if(res.get(i).size()==edges.length){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        //adjacency matrix
+        // int n=edges.length+1;
+        // int[][] matrix=new int[n+1][n+1];
+        // for(int[] e:edges){
+        //     int u=e[0];
+        //     int v=e[1];
+        //     matrix[u][v]=1;
+        //     matrix[v][u]=1;
+        // }
+        // for(int i=1;i<=n;i++){
+        //     int degree=0;
+        //     for(int j=1;j<=n;j++){
+        //         degree+=matrix[i][j];
+        //     }
+        //     if(degree==n-1) return i;
+        // }
+        // return -1;
+
+        //using degree
         int n=edges.length+1;
-        ArrayList<ArrayList<Integer>> res=new ArrayList<>();
-        for(int i=0;i<=n;i++){
-            res.add(new ArrayList<>());
-        }
-        for (int[] e : edges) {
-            int u = e[0];
-            int v = e[1];
-            res.get(u).add(v);
-            res.get(v).add(u);
+        int[] degree=new int[n+1];
+        for(int[] e:edges){
+            int u=e[0];
+            int v=e[1];
+            degree[u]++;
+            degree[v]++;
         }
         for(int i=1;i<=n;i++){
-            if(res.get(i).size()==edges.length){
+            if(degree[i]==n-1){
                 return i;
             }
         }
