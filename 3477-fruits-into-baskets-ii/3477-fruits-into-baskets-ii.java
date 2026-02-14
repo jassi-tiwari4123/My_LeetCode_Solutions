ff40fdbec80167ccc27eed642ead04abb865d64c
Jassi Tiwari <jassitiwari550@gmail.com>
2026-02-14 23:15:50 +0530

    Time: 1 ms (99.95%), Space: 46.5 MB (67.86%) - LeetHub

--- 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.java
+++ 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.java
@@ -1,23 +1,41 @@
 class Solution {
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
-        int n=fruits.length;
-        ArrayList<Integer> b=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            b.add(baskets[i]);
-        }
-        int res=0;
+        // int n=fruits.length;
+        // ArrayList<Integer> b=new ArrayList<>();
+        // for(int i=0;i<n;i++){
+        //     b.add(baskets[i]);
+        // }
+        // int res=0;
         
-        for(int i=0;i<n;i++){
-            int fruit=fruits[i];
-            boolean place=false;
-            for(int j=0;j<b.size();j++){
-                if(fruit<=b.get(j)){
-                    place=true;
-                    b.remove(j);
+        // for(int i=0;i<n;i++){
+        //     int fruit=fruits[i];
+        //     boolean place=false;
+        //     for(int j=0;j<b.size();j++){
+        //         if(fruit<=b.get(j)){
+        //             place=true;
+        //             b.remove(j);
+        //             break;
+        //         }
+        //     }
+        //     if(!place){
+        //         res++;
+        //     }
+        // }
+        // return res;
+
+
+
+        for(int i=0;i<fruits.length;i++){
+            for(int j=0;j<baskets.length;j++){
+                if(baskets[j]>=fruits[i]){
+                    baskets[j]=-1;
                     break;
                 }
             }
-            if(!place){
+        }
+        int res=0;
+        for(int i=0;i<baskets.length;i++){
+            if(baskets[i]!=-1){
                 res++;
             }
         }
