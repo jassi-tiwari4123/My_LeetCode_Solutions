c8273ec9af12a2f64f85f4b187bde2868cb723c2
Jassi Tiwari <jassitiwari550@gmail.com>
2026-02-15 09:58:15 +0530

    Time: 2 ms (99.39%), Space: 45.4 MB (23.74%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -6,12 +6,12 @@ class Solution {
         return res;
     }
     public void backTrack(int start,int[] nums,List<Integer> op,List<List<Integer>> res){
-        res.add(new ArrayList<>(op));
         for(int i=start;i<nums.length;i++){
             if(i>start && nums[i]==nums[i-1]) continue;
             op.add(nums[i]);
             backTrack(i+1,nums,op,res);
             op.remove(op.size()-1);
         }
+        res.add(new ArrayList<>(op));
     }
 }
