6a9150f7d72d011ff49dcfdadefb518f6cba7826
Jassi Tiwari <jassitiwari550@gmail.com>
2026-02-17 22:50:47 +0530

    Time: 1 ms (99.81%), Space: 47.2 MB (87.44%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,21 +1,41 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            nums[i]=nums[i]*nums[i];
-        }
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j>0;j--){
-                if(nums[j]<nums[j-1]){
-                    int temp=nums[j];
-                    nums[j]=nums[j-1];
-                    nums[j-1]=temp;
-                }
-                else{
-                    break;
-                }
+        // for(int i=0;i<nums.length;i++){
+        //     nums[i]=nums[i]*nums[i];
+        // }
+        // for(int i=0;i<nums.length-1;i++){
+        //     for(int j=i+1;j>0;j--){
+        //         if(nums[j]<nums[j-1]){
+        //             int temp=nums[j];
+        //             nums[j]=nums[j-1];
+        //             nums[j-1]=temp;
+        //         }
+        //         else{
+        //             break;
+        //         }
+        //     }
+        // }
+
+        int l=0;
+        int n=nums.length;
+        int r=n-1;
+        int pos=n-1;
+        int[] res=new int[n];
+        while(l<=r){
+            int left=nums[l]*nums[l];
+            int right=nums[r]*nums[r];
+            if(left>right){
+                res[pos]=left;
+                l++;
             }
+            else{
+                res[pos]=right;
+                r--;
+            }
+            pos--;
+            
         }
         
-        return nums;    
+        return res;    
     }
 }
