626a37ed1987b1fccc9f5f230edc7106610d9dec
Jassi Tiwari <jassitiwari550@gmail.com>
2026-03-10 16:36:31 +0530

    Time: 14 ms (87.05%), Space: 47.4 MB (83.45%) - LeetHub

---
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int maxDistance(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 1){
+                    q.add(new int[]{i, j});
+                }
+            }
+        }
+
+        if(q.isEmpty() || q.size() == n * m) return -1;
+        
+        int[] dirRow = {1, -1, 0, 0};
+        int[] dirCol = {0, 0, -1, 1};
+        int dist = -1;
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            dist++;
+            for(int i = 0; i < size; i++){
+                int[] cur = q.poll();
+                int row = cur[0];
+                int col = cur[1];
+                for(int k = 0; k < 4; k++){
+                    int newRow = row + dirRow[k];
+                    int newCol = col + dirCol[k];
+                    if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && grid[newRow][newCol] == 0){
+                        grid[newRow][newCol] = 1;
+                        q.add(new int[]{newRow, newCol});
+                    }
+                }
+            }
+        }
+        
+        return dist;
+    }
+}
