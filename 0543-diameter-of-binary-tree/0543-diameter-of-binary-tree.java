010a5fa316f0608f48aa41770b1f1690b5b34d53
Jassi Tiwari <jassitiwari550@gmail.com>
2026-03-10 12:01:00 +0530

    Time: 0 ms (100%), Space: 46.6 MB (97.35%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -13,37 +13,38 @@
  *     }
  * }
  */
-
-// class Solution {
-//     int diameter=0;
-//     public int diameterOfBinaryTree(TreeNode root){ 
-//         depth(root);
-//         return diameter;
-//     }
-//     public int depth(TreeNode root){
-//         if(root==null) return 0;
-//         int left=depth(root.left);
-//         int right=depth(root.right);
-//         diameter=Math.max(diameter,left+right);
-//         return 1+Math.max(left,right);
-//     }
-// }
-
-
+//o(n)
 class Solution {
-
-    public int diameterOfBinaryTree(TreeNode root) {
-        if(root == null) return 0;
-        int leftHeight = height(root.left);
-        int rightHeight = height(root.right);
-        int currentDiameter = leftHeight + rightHeight;
-        int leftDiameter = diameterOfBinaryTree(root.left);
-        int rightDiameter = diameterOfBinaryTree(root.right);
-
-        return Math.max(currentDiameter, Math.max(leftDiameter, rightDiameter));
+    int diameter=0;
+    public int diameterOfBinaryTree(TreeNode root){ 
+        depth(root);
+        return diameter;
     }
-    public int height(TreeNode root){
-        if(root == null) return 0;
-        return 1 + Math.max(height(root.left), height(root.right));
+    public int depth(TreeNode root){
+        if(root==null) return 0;
+        int left=depth(root.left);
+        int right=depth(root.right);
+        diameter=Math.max(diameter,left+right);
+        return 1+Math.max(left,right);
     }
 }
+
+
+// o(n^2)
+// class Solution {
+
+//     public int diameterOfBinaryTree(TreeNode root) {
+//         if(root == null) return 0;
+//         int leftHeight = height(root.left);
+//         int rightHeight = height(root.right);
+//         int currentDiameter = leftHeight + rightHeight;
+//         int leftDiameter = diameterOfBinaryTree(root.left);
+//         int rightDiameter = diameterOfBinaryTree(root.right);
+
+//         return Math.max(currentDiameter, Math.max(leftDiameter, rightDiameter));
+//     }
+//     public int height(TreeNode root){
+//         if(root == null) return 0;
+//         return 1 + Math.max(height(root.left), height(root.right));
+//     }
+// }
