c36d65d43374233686b9319dbe4269b0e1a04cd3
Jassi Tiwari <jassitiwari550@gmail.com>
2026-03-10 11:59:37 +0530

    Time: 201 ms (5.21%), Space: 48.9 MB (5.23%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -13,17 +13,37 @@
  *     }
  * }
  */
+
+// class Solution {
+//     int diameter=0;
+//     public int diameterOfBinaryTree(TreeNode root){ 
+//         depth(root);
+//         return diameter;
+//     }
+//     public int depth(TreeNode root){
+//         if(root==null) return 0;
+//         int left=depth(root.left);
+//         int right=depth(root.right);
+//         diameter=Math.max(diameter,left+right);
+//         return 1+Math.max(left,right);
+//     }
+// }
+
+
 class Solution {
-    int diameter=0;
-    public int diameterOfBinaryTree(TreeNode root){ 
-        depth(root);
-        return diameter;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root == null) return 0;
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        int currentDiameter = leftHeight + rightHeight;
+        int leftDiameter = diameterOfBinaryTree(root.left);
+        int rightDiameter = diameterOfBinaryTree(root.right);
+
+        return Math.max(currentDiameter, Math.max(leftDiameter, rightDiameter));
     }
-    public int depth(TreeNode root){
-        if(root==null) return 0;
-        int left=depth(root.left);
-        int right=depth(root.right);
-        diameter=Math.max(diameter,left+right);
-        return 1+Math.max(left,right);
+    public int height(TreeNode root){
+        if(root == null) return 0;
+        return 1 + Math.max(height(root.left), height(root.right));
     }
 }
