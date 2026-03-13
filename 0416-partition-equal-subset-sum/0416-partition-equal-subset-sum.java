8b82f5443d53431f03094c92392fe8cbabd96b74
Jassi Tiwari <jassitiwari550@gmail.com>
2026-03-13 10:21:11 +0530

    Time: 96 ms (20.21%), Space: 72.8 MB (26.11%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -0,0 +1,86 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        }
+        if(sum%2!=0) return false;
+        int target=sum/2;
+        Boolean[][] dp=new Boolean[nums.length][target+1];
+        return fxn(nums,0,target,dp); 
+    }
+    public boolean fxn(int[]nums,int ind,int target,Boolean[][]dp){
+        if(target==0) return true;
+        if(target<0 || ind>=nums.length) return false;
+        if(dp[ind][target]!=null) return dp[ind][target]; 
+        boolean take=fxn(nums,ind+1,target-nums[ind],dp);
+        boolean notTake=fxn(nums,ind+1,target,dp);
+        return dp[ind][target]=take||notTake;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+
+//     Boolean[][] dp;
+
+//     public boolean canPartition(int[] nums) {
+
+//         int sum = 0;
+//         for(int x : nums) sum += x;
+
+//         if(sum % 2 != 0) return false;
+
+//         int target = sum / 2;
+
+//         dp = new Boolean[nums.length][target + 1];
+
+//         return solve(nums, 0, target);
+//     }
+
+//     public boolean solve(int[] nums, int i, int target){
+
+//         if(target == 0) return true;
+
+//         if(i >= nums.length || target < 0) return false;
+
+//         if(dp[i][target] != null) return dp[i][target];
+
+//         boolean take = solve(nums, i + 1, target - nums[i]);
+//         boolean skip = solve(nums, i + 1, target);
+
+//         return dp[i][target] = take || skip;
+//     }
+// }
