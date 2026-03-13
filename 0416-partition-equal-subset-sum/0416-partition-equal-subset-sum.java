95ef699d32c29653149641d86ef0b9c3f526c8fe
Jassi Tiwari <jassitiwari550@gmail.com>
2026-03-13 10:54:50 +0530

    Time: 52 ms (43.61%), Space: 47.5 MB (60.38%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,21 +1,49 @@
 class Solution {
     public boolean canPartition(int[] nums) {
+        //memoization
+    //     int sum=0;
+    //     for(int i=0;i<nums.length;i++){
+    //         sum+=nums[i];
+    //     }
+    //     if(sum%2!=0) return false;
+    //     int target=sum/2;
+    //     Boolean[][] dp=new Boolean[nums.length][target+1];
+    //     return fxn(nums,0,target,dp); 
+    // }
+    // public boolean fxn(int[]nums,int ind,int target,Boolean[][]dp){
+    //     if(target==0) return true;
+    //     if(target<0 || ind>=nums.length) return false;
+    //     if(dp[ind][target]!=null) return dp[ind][target]; 
+    //     boolean take=fxn(nums,ind+1,target-nums[ind],dp);
+    //     boolean notTake=fxn(nums,ind+1,target,dp);
+    //     return dp[ind][target]=take||notTake;
+
+
+    //tabulation
         int sum=0;
-        for(int i=0;i<nums.length;i++){
+        int n=nums.length;
+        for(int i=0;i<n;i++){
             sum+=nums[i];
         }
         if(sum%2!=0) return false;
         int target=sum/2;
-        Boolean[][] dp=new Boolean[nums.length][target+1];
-        return fxn(nums,0,target,dp); 
-    }
-    public boolean fxn(int[]nums,int ind,int target,Boolean[][]dp){
-        if(target==0) return true;
-        if(target<0 || ind>=nums.length) return false;
-        if(dp[ind][target]!=null) return dp[ind][target]; 
-        boolean take=fxn(nums,ind+1,target-nums[ind],dp);
-        boolean notTake=fxn(nums,ind+1,target,dp);
-        return dp[ind][target]=take||notTake;
+        boolean[][] dp=new boolean[n+1][target+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0]=true;
+        }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=target;j++){
+                boolean notTake=dp[i-1][j];
+                boolean take=false;
+                if(nums[i-1]<=j){
+                    take=dp[i-1][j-nums[i-1]];
+                }
+                dp[i][j]=take||notTake;
+            }
+            
+        }
+        return dp[n][target];
     }
 }
 
