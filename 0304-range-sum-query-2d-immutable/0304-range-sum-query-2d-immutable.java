fb3faff7912de52d663ecb42752d3249a86131e9
Jassi Tiwari <jassitiwari550@gmail.com>
2026-03-17 14:15:37 +0530

    Time: 101 ms (83.94%), Space: 142.2 MB (43.44%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -1,30 +1,20 @@
 class NumMatrix {
-    int[][] pref;
-
+    int[][] prefSum;
     public NumMatrix(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        pref = new int[n + 1][m + 1];
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                pref[i][j] =
-                    pref[i - 1][j]
-                  + pref[i][j - 1]
-                  - pref[i - 1][j - 1]
-                  + matrix[i - 1][j - 1];
+        int n=matrix.length;
+        int m=matrix[0].length;
+        prefSum=new int[n+1][m+1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                prefSum[i+1][j+1]=prefSum[i+1][j]+prefSum[i][j+1]-prefSum[i][j]+matrix[i][j];
             }
         }
     }
-
-    public int sumRegion(int r1, int c1, int r2, int c2) {
-        return pref[r2 + 1][c2 + 1]
-             - pref[r1][c2 + 1]
-             - pref[r2 + 1][c1]
-             + pref[r1][c1];
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return prefSum[row2+1][col2+1]-prefSum[row1][col2+1]-prefSum[row2+1][col1]+prefSum[row1][col1];
     }
 }
-
 
 /**
  * Your NumMatrix object will be instantiated and called as such:
