ebce62090600cd808038e3ddf582af6deca435b9
Jassi Tiwari <jassitiwari550@gmail.com>
2026-03-31 20:58:14 +0530

    Time: 7 ms (58.41%), Space: 43.8 MB (96.34%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.java
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.java
@@ -1,20 +1,39 @@
 class Solution {
     public long pickGifts(int[] gifts, int k) {
-        int n=gifts.length;
-        Arrays.sort(gifts);
+        //my approach
+        // int n=gifts.length;
+        // Arrays.sort(gifts);
+        // while(k-->0){
+        //     int idx=0;
+        //     for(int i=1;i<n;i++){
+        //         if(gifts[i]>gifts[idx]){
+        //             idx=i;
+        //         }
+        //     }
+        //     gifts[idx]=(int)Math.sqrt(gifts[idx]);
+        // }
+        // long sum=0;
+        // for(int i=0;i<n;i++){
+        //     sum+=gifts[i];
+        // }
+        // return sum;
+
+
+        //can use priority queue as well for top k elements
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<gifts.length;i++){
+            pq.add(gifts[i]);
+        }
+
         while(k-->0){
-            int idx=0;
-            for(int i=1;i<n;i++){
-                if(gifts[i]>gifts[idx]){
-                    idx=i;
-                }
-            }
-            gifts[idx]=(int)Math.sqrt(gifts[idx]);
+            int x=pq.poll();
+            int sq=(int)Math.sqrt(x);
+            pq.add(sq);
         }
-        long sum=0;
-        for(int i=0;i<n;i++){
-            sum+=gifts[i];
+        long res=0;
+        while(!pq.isEmpty()){
+            res+=pq.poll();
         }
-        return sum;
+        return res;
     }
 }
