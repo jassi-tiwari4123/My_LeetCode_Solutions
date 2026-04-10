3500d9de2c23e9732ced612be6dd517473447659
Jassi Tiwari <jassitiwari550@gmail.com>
2026-04-10 19:11:52 +0530

    Time: 1078 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,2 +1,6 @@
 # Write your MySQL query statement below
-select id from weather w where temperature>(select temperature from weather where recorddate=date_sub(w.recorddate,interval 1 day));
+-- select id from weather w where temperature>(select temperature from weather where recorddate=
+-- date_sub(w.recorddate,interval 1 day));
+
+
+select w.Id from weather w where w.temperature>(select temperature from weather where recordDate=date_sub(w.recordDate,interval 1 day));
