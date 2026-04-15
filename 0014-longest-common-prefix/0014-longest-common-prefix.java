314495ce1965dd3d05e4afd038a0d35395a8c0ab
Jassi Tiwari <jassitiwari550@gmail.com>
2026-04-15 16:20:14 +0530

    Time: 1 ms (63.31%), Space: 43.1 MB (85.57%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,28 +1,29 @@
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
-        // if (strs == null || strs.length == 0) return "";
-
-        // for (int i = 0; i < strs[0].length(); i++) {
-        //     char ch = strs[0].charAt(i);
-        //     for (int j = 1; j < strs.length; j++) {
-        //         if (i >= strs[j].length() || strs[j].charAt(i) != ch) {
-        //             return strs[0].substring(0, i);
+        //complexity: O(n*m); where n is length of largest string and m is smallest
+        //brute and vertical scanning approach best for interview
+        // int n=strs.length;
+        // if(n==0 || strs==null) return "";
+        // for(int i=0;i<strs[0].length();i++){
+        //     char ch=strs[0].charAt(i);
+        //     for(int j=1;j<n;j++){
+        //         if(i>=strs[j].length() || strs[j].charAt(i)!=ch){
+        //             return strs[0].substring(0,i);
         //         }
         //     }
         // }
         // return strs[0];
 
 
+        //optimise but sue of sorting O(n log n)
         int n=strs.length;
-        if(n==0 || strs==null) return "";
-        for(int i=0;i<strs[0].length();i++){
-            char ch=strs[0].charAt(i);
-            for(int j=1;j<n;j++){
-                if(i>=strs[j].length() || strs[j].charAt(i)!=ch){
-                    return strs[0].substring(0,i);
-                }
-            }
+        Arrays.sort(strs);
+        String first=strs[0];
+        String last=strs[n-1];
+        int i=0;
+        while(i<first.length() && i<last.length() && first.charAt(i)==last.charAt(i)){
+            i++;
         }
-        return strs[0];
+        return first.substring(0,i);
     }
 }
