faf2916cfca66ac75e62f73d6466cd3cc1a4ddd3
Jassi Tiwari <jassitiwari550@gmail.com>
2026-04-15 18:27:57 +0530

    Time: 2 ms (69.62%), Space: 44.2 MB (23.83%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
@@ -5,13 +5,16 @@ class Solution {
             hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
         }
         int maxim=0;
-        for(int i:hm.values()){
-            maxim=Math.max(maxim,i);
-        }
         int count=0;
-        for(int i:hm.values()){
-            if(i==maxim){
-                count+=i;
+        for(Map.Entry<Integer,Integer> e:hm.entrySet()){
+            int el=e.getKey();
+            int fr=e.getValue();
+            if(maxim<fr){
+                maxim=fr;
+                count=fr;
+            }
+            else if(maxim==fr){
+                count+=fr;
             }
         }
         return count;
