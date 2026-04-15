e095b80d764face036c2a3fa3755a20fd0ea7af0
Jassi Tiwari <jassitiwari550@gmail.com>
2026-04-15 13:03:48 +0530

    Time: 75 ms (0.85%), Space: 42.3 MB (89.3%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,30 +1,28 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        // Brian Kernighan’s algorithm counts set bits 
+        // Brian Kernighan’s algorithm counts set bits
+        //TC-O(1) SC-O(1) 
         // if(n<=0) return false;
         // return (n & (n-1))==0;
 
 
-        //basic logic
-        // if(n<=0) return false;
-        // int count=0;
-        // while(n!=0){
-        //     int z=n & 1;
-        //     if(z==1){
-        //         count++;
-        //     }
-        //     n=n>>1;
-        // }
-        // if(count!=1){
-        //     return false;
-        // }
-        // return true;
-
-        //brute O(log n)
+        //basic logic 
         if(n<=0) return false;
-        while(n%2==0){
-            n=n/2;
+        int count=0;
+        while(n!=0){
+            int z=n & 1;
+            if(z==1){
+                count++;
+            }
+            n=n>>1;
         }
-        return n==1;
+        return count==1;
+
+        //brute TC-O(log n) SC-O(1) 
+        // if(n<=0) return false;
+        // while(n%2==0){
+        //     n=n/2;
+        // }
+        // return n==1;
     }
 }
