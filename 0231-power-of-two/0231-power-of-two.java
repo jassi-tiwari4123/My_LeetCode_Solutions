2b94b9a020c59dfba29dbaf18cce0b1a0cce5b37
Jassi Tiwari <jassitiwari550@gmail.com>
2026-04-15 12:59:11 +0530

    Time: 1 ms (96.53%), Space: 42.6 MB (42.15%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -3,18 +3,28 @@ class Solution {
         // Brian Kernighan’s algorithm counts set bits 
         // if(n<=0) return false;
         // return (n & (n-1))==0;
+
+
+        //basic logic
+        // if(n<=0) return false;
+        // int count=0;
+        // while(n!=0){
+        //     int z=n & 1;
+        //     if(z==1){
+        //         count++;
+        //     }
+        //     n=n>>1;
+        // }
+        // if(count!=1){
+        //     return false;
+        // }
+        // return true;
+
+        //brute O(log n)
         if(n<=0) return false;
-        int count=0;
-        while(n!=0){
-            int z=n & 1;
-            if(z==1){
-                count++;
-            }
-            n=n>>1;
+        while(n%2==0){
+            n=n/2;
         }
-        if(count!=1){
-            return false;
-        }
-        return true;
+        return n==1;
     }
 }
