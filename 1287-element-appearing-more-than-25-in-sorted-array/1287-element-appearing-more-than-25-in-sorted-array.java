5a88e35b55e086c48f329ea2d7f0ee9ccc905552
Jassi Tiwari <jassitiwari550@gmail.com>
2026-04-15 10:10:34 +0530

    Time: 13 ms (23%), Space: 47.1 MB (20.15%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.java
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.java
@@ -1,16 +1,30 @@
 class Solution {
     public int findSpecialInteger(int[] arr) {
-        double r=arr.length*0.25;
+        // double r=arr.length*0.25;
+        // HashMap<Integer,Integer> hm=new HashMap<>();
+        // for(int i=0;i<arr.length;i++){
+        //     hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
+        // }
+        // int ans=0;
+        // for(Map.Entry<Integer,Integer> entry:hm.entrySet()){
+        //     if(entry.getValue()>r){
+        //         ans=entry.getKey();
+        //     }
+        // }
+        // return ans;
+
+        int n=arr.length;
         HashMap<Integer,Integer> hm=new HashMap<>();
-        for(int i=0;i<arr.length;i++){
+        for(int i=0;i<n;i++){
             hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
         }
-        int ans=0;
-        for(Map.Entry<Integer,Integer> entry:hm.entrySet()){
-            if(entry.getValue()>r){
-                ans=entry.getKey();
+        for(Map.Entry<Integer,Integer> e:hm.entrySet()){
+            int el=e.getKey();
+            int freq=e.getValue();
+            if(freq>n/4){
+                return el;
             }
         }
-        return ans;
+        return -1;
     }
 }
