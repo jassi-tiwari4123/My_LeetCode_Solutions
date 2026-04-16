6bcc538ec913f34648c136565d8e33d0745a9056
Jassi Tiwari <jassitiwari550@gmail.com>
2026-04-16 08:42:28 +0530

    Time: 3 ms (93.14%), Space: 45.5 MB (24.92%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,28 +1,20 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> nextGreater = new HashMap<>();
-        Stack<Integer> stack = new Stack<>();
-
-        // Traverse nums2 and compute next greater for each element
-        for (int num : nums2) {
-            // Pop smaller elements and assign their next greater
-            while (!stack.isEmpty() && num > stack.peek()) {
-                nextGreater.put(stack.pop(), num);
+        Map<Integer,Integer> hm=new HashMap<>();
+        Stack<Integer> st=new Stack<>();
+        for(int num:nums2){
+            while(!st.isEmpty() && st.peek()<num){
+                hm.put(st.pop(),num);
             }
-            stack.push(num);
+            st.push(num);
         }
-
-        // Elements left in stack have no greater element → assign -1
-        while (!stack.isEmpty()) {
-            nextGreater.put(stack.pop(), -1);
+        while(!st.isEmpty()){
+            hm.put(st.pop(),-1);
         }
-
-        // Build result for nums1 based on computed map
-        int[] res = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            res[i] = nextGreater.get(nums1[i]);
+        int[] res=new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            res[i]=hm.get(nums1[i]);
         }
-
         return res;
     }
 }
