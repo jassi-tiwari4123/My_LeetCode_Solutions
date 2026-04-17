c7d509aac355b52c60c45b781287c847e08e5f05
Jassi Tiwari <jassitiwari550@gmail.com>
2026-04-17 23:22:36 +0530

    Time: 1 ms (100%), Space: 48.5 MB (96.78%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -23,8 +23,7 @@ class Solution {
 
 
         //optimised TC-O(log(n+m))
-        int n=nums1.length;
-        int m=nums2.length;
+        int n=nums1.length;int m=nums2.length;
         if(n>m){
             return findMedianSortedArrays(nums2,nums1);
         }
@@ -33,14 +32,10 @@ class Solution {
         int low=0;
         int high=n;
         while(low<=high){
-            int mid1=low+(high-low)/2;
-            int mid2=left-mid1;
-            int l1=Integer.MIN_VALUE;
-            int l2=Integer.MIN_VALUE;
-            int r1=Integer.MAX_VALUE;
-            int r2=Integer.MAX_VALUE;
-            if(mid1<n) r1=nums1[mid1];
-            if(mid2<m) r2=nums2[mid2];
+            int mid1=low+(high-low)/2;int mid2=left-mid1;
+            int l1=Integer.MIN_VALUE;int l2=Integer.MIN_VALUE;
+            int r1=Integer.MAX_VALUE;int r2=Integer.MAX_VALUE;
+            if(mid1<n) r1=nums1[mid1];if(mid2<m) r2=nums2[mid2];
             if(mid1-1>=0) l1=nums1[mid1-1];
             if(mid2-1>=0) l2=nums2[mid2-1];
             if(l1<=r2 && l2<=r1){
@@ -49,12 +44,8 @@ class Solution {
                 }
                 return (double)(Math.max(l1,l2)+Math.min(r1,r2))/2.0;
             }
-            else if(l1>r2){
-                high=mid1-1;
-            }
-            else{
-                low=mid1+1;
-            }
+            else if(l1>r2) high=mid1-1;
+            else low=mid1+1;
         }
         return 0.0;
     }
