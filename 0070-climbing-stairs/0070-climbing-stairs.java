74b5a5f5d2c6ad0046c2b5d8c00a40786fca2e1f
Jassi Tiwari <jassitiwari550@gmail.com>
2026-04-23 14:30:39 +0530

    Time: 0 ms (100%), Space: 42.2 MB (38.15%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,29 @@
 class Solution {
     public int climbStairs(int n) {
-        int [] dp=new int[n+1];
+    //     int [] dp=new int[n+1];
+    //     Arrays.fill(dp,-1);
+    //     int res=compute(n,dp);
+    //     return res;
+    // }
+    // public int compute(int n,int[] dp){
+    //     if(n<0) return 0;
+    //     if(n==0) return 1;
+    //     if(dp[n]!=-1) return dp[n];
+    //     int one=compute(n-1,dp);
+    //     int two=compute(n-2,dp);
+    //     return dp[n]=one + two;
+
+
+        int[] dp=new int[n+1];
         Arrays.fill(dp,-1);
-        int res=compute(n,dp);
-        return res;
+        return fxn(n,dp);
     }
-    public int compute(int n,int[] dp){
+    public int fxn(int n,int[]dp){
         if(n<0) return 0;
         if(n==0) return 1;
         if(dp[n]!=-1) return dp[n];
-        int one=compute(n-1,dp);
-        int two=compute(n-2,dp);
-        return dp[n]=one + two;
+        int one=fxn(n-1,dp);
+        int two=fxn(n-2,dp);
+        return dp[n]=one+two;
     }
 }
