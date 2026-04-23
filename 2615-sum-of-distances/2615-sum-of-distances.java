3a8aedf72e4cd0a835f44a2c98aeb7b81c87811e
Jassi Tiwari <jassitiwari550@gmail.com>
2026-04-23 13:54:50 +0530

    Time: 22 ms (73.54%), Space: 121.8 MB (49.33%) - LeetHub

--- 2615-sum-of-distances/2615-sum-of-distances.java
+++ 2615-sum-of-distances/2615-sum-of-distances.java
@@ -1,3 +1,8 @@
+//simply humne observe kiya ki left handside kitne occurence hain selected element ka and right handside kitne
+//1,2,1,1,1,1,2,1
+//left handside for index 2--> 2 times 1 and rhs 3 times 1
+//for lhs= i-0+i-2+i==> 2*i+(sum of index)
+//for rhs= 4-i+5-i+7-i==> (sum of index-(3*i))
 class Solution {
     public long[] distance(int[] nums) {
         int n=nums.length;
@@ -8,17 +13,23 @@ class Solution {
             hm.get(nums[i]).add(i);
         }
         for(ArrayList<Integer> l:hm.values()){
+            long sumL=0;
+            long countL=0;
             int size=l.size();
-            long[] pref=new long[size+1];
-            for(int i=0;i<size;i++){
-                pref[i+1]=pref[i]+l.get(i);
-            }
-            for(int i=0;i<size;i++){
-                int ind=l.get(i);
-                long lf=(long)i*ind-pref[i];
-                long r=(pref[size]-pref[i+1])-(long)(size-i-1)*ind;
-                res[ind]=lf+r;
-            }
+            long total=0;
+            for(int i:l) total+=i;
+            long sumR=total;
+            long countR=size;
+            for(int j=0;j<size;j++){
+                int idx=l.get(j);
+                sumR-=idx;
+                countR--;
+                long left=countL*idx-sumL;
+                long right=sumR-countR*idx;
+                res[idx]=left+right;
+                sumL+=idx;
+                countL++;
+            } 
         }
         return res;
     }
