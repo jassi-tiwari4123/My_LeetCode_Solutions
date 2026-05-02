08e97b1428ef00d8a3acd79fa44e996fbdc20474
Jassi Tiwari <jassitiwari550@gmail.com>
2026-05-02 20:49:27 +0530

    Time: 1 ms (66.86%), Space: 45.5 MB (55.01%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        int n=s.length();
+        char[] res=new char[n];
+        for(int i=0;i<n;i++){
+            res[indices[i]]=s.charAt(i);
+        }
+        return new String(res);
+    }
+}
