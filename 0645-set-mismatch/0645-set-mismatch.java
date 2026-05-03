60dacf6f35b9af1e725c84d2642e9d6bbc6d4459
Jassi Tiwari <jassitiwari550@gmail.com>
2026-05-03 23:20:42 +0530

    Time: 14 ms (33.33%), Space: 48 MB (18.46%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,21 +1,40 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
+        //my approach
         int n=nums.length;
         int dup=-1;
+        HashSet<Integer> s=new HashSet<>();
         for(int i=0;i<n;i++){
-            int idx=Math.abs(nums[i])-1;
-            if(nums[idx]<0){
-                dup=Math.abs(nums[i]);
+            if(s.contains(nums[i])){
+                dup=nums[i];
             }
-            else{
-                nums[idx]*=-1;
+            s.add(nums[i]);
+        }
+        int mis=-1;
+        for(int i=1;i<=n;i++){
+            if(!s.contains(i)){
+                mis=i;
             }
         }
-        for(int i=0;i<n;i++){
-            if(nums[i]>0){
-                return new int[]{dup,i+1};
-            }
-        }
-        return new int[]{-1,-1};
+        return new int[]{dup,mis};
+
+
+        // int n=nums.length;
+        // int dup=-1;
+        // for(int i=0;i<n;i++){
+        //     int idx=Math.abs(nums[i])-1;
+        //     if(nums[idx]<0){
+        //         dup=Math.abs(nums[i]);
+        //     }
+        //     else{
+        //         nums[idx]*=-1;
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]>0){
+        //         return new int[]{dup,i+1};
+        //     }
+        // }
+        // return new int[]{-1,-1};
     }
 }
