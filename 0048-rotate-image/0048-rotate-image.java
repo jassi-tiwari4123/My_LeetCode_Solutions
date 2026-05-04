5a6c3c1f3abac8fcc52e201ad2d7483e4b5eda95
Jassi Tiwari <jassitiwari550@gmail.com>
2026-05-04 11:44:12 +0530

    Time: 0 ms (100%), Space: 43.4 MB (92.59%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,42 +1,29 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        //it is using extra space
-        // int n=matrix.length;
-        // int[][] newMat=new int[n][n];
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<n;j++){
-        //         newMat[i][j]=matrix[n-j-1][i];
-        //     }
-        // }
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<n;j++){
-        //         matrix[i][j]=newMat[i][j];
-        //     }
-        // }
-
-
-        //can do transpose and then reverse each row
+       //transpose and reverse the row
         int n=matrix.length;
         int m=matrix[0].length;
         for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
+            for(int j=i;j<m;j++){
                 int temp=matrix[i][j];
                 matrix[i][j]=matrix[j][i];
                 matrix[j][i]=temp;
             }
         }
         for(int i=0;i<n;i++){
-            reverse(i,0,n-1,matrix);
+            reverse(i,0,m-1,matrix);
         }
     }
-
-    public void reverse(int i,int l,int r,int[][]matrix){
-        while(l<r){
-            int temp=matrix[i][l];
-            matrix[i][l]=matrix[i][r];
-            matrix[i][r]=temp;
-            l++;
-            r--;
+    public void reverse(int r,int i,int j,int[][] matrix){
+        while(i<=j){
+            int temp=matrix[r][i];
+            matrix[r][i]=matrix[r][j];
+            matrix[r][j]=temp;
+            i++;
+            j--;
         }
     }
 }
+
+
+ 
