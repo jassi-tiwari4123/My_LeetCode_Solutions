73725b4309dd6e961902cf33449db5c4d589be09
Jassi Tiwari <jassitiwari550@gmail.com>
2026-05-06 00:27:34 +0530

    Time: 89 ms (20.16%), Space: 48.8 MB (29.59%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,36 +1,36 @@
 class Solution {
     public String removeStars(String s) {
         //APPROACH - 1
-        // int n=s.length();
-        // Stack<Character> st=new Stack<>();
-        // for(int i=0;i<n;i++){
-        //     if(s.charAt(i)!='*'){
-        //         st.push(s.charAt(i));
-        //     }
-        //     else{
-        //         if(!st.isEmpty()){
-        //             st.pop();
-        //         }
-        //     }
-        // }
-        // StringBuilder sb=new StringBuilder();
-        // while(!st.isEmpty()){
-        //     sb.append(st.pop());
-        // }
-        // return sb.reverse().toString();
-
-        //WITHOUT STACK 
         int n=s.length();
-        StringBuilder sb=new StringBuilder();
+        Stack<Character> st=new Stack<>();
         for(int i=0;i<n;i++){
-            if(s.charAt(i)=='*'){
-                sb.deleteCharAt(sb.length()-1);
+            if(s.charAt(i)!='*'){
+                st.push(s.charAt(i));
             }
             else{
-                sb.append(s.charAt(i));
+                if(!st.isEmpty()){
+                    st.pop();
+                }
             }
-
         }
-        return sb.toString();
+        StringBuilder sb=new StringBuilder();
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+        return sb.reverse().toString();
+
+        //WITHOUT STACK 
+        // int n=s.length();
+        // StringBuilder sb=new StringBuilder();
+        // for(int i=0;i<n;i++){
+        //     if(s.charAt(i)=='*'){
+        //         sb.deleteCharAt(sb.length()-1);
+        //     }
+        //     else{
+        //         sb.append(s.charAt(i));
+        //     }
+
+        // }
+        // return sb.toString();
     }
 }
