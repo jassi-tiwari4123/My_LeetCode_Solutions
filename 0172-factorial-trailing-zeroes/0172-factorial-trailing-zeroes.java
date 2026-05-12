200f07c067b3dbf341a8e9e581f2fe26ac3dd43b
Jassi Tiwari <jassitiwari550@gmail.com>
2026-05-12 10:19:02 +0530

    Time: 0 ms (100%), Space: 42.1 MB (92.29%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int trailingZeroes(int num) {
+        int count=0;
+        while(num>0){
+            num=num/5;
+            count+=num;
+        }
+        return count;
+    }
+}
