33fe06bf8aedc6b9056cd0ffcc5610a11153b219
Jassi Tiwari <jassitiwari550@gmail.com>
2026-05-17 11:51:51 +0530

    Time: 4 ms (82.28%), Space: 46.4 MB (39.95%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,31 +1,31 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // int n=s.length();
-        // int m=t.length();
-        // if(n!=m) return false;
-        // HashMap<Character,Integer> hm=new HashMap<>();
-        // for(int i=0;i<n;i++){
-        //     hm.put(s.charAt(i),hm.getOrDefault(s.charAt(i),0)+1);
-        // }
-        // for(int i=0;i<m;i++){
-        //     if(!hm.containsKey(t.charAt(i))) return false;
-        //     hm.put(t.charAt(i),hm.get(t.charAt(i))-1);
-        //     if(hm.get(t.charAt(i))==0){
-        //         hm.remove(t.charAt(i));
-        //     }
-        // }
-        // return hm.size()==0;
+        // // int n=s.length();
+        // // int m=t.length();
+        // // if(n!=m) return false;
+        // // HashMap<Character,Integer> hm=new HashMap<>();
+        // // for(int i=0;i<n;i++){
+        // //     hm.put(s.charAt(i),hm.getOrDefault(s.charAt(i),0)+1);
+        // // }
+        // // for(int i=0;i<m;i++){
+        // //     if(!hm.containsKey(t.charAt(i))) return false;
+        // //     hm.put(t.charAt(i),hm.get(t.charAt(i))-1);
+        // //     if(hm.get(t.charAt(i))==0){
+        // //         hm.remove(t.charAt(i));
+        // //     }
+        // // }
+        // // return hm.size()==0;
 
         //basic
-        char[] c1=s.toCharArray();
-        char[] c2=t.toCharArray();
-        Arrays.sort(c1);
-        Arrays.sort(c2);
-        int n=c1.length;
-        int m=c2.length;
-        if(n!=m) return false;
-        for(int i=0;i<n;i++){
-            if(c1[i]!=c2[i]) return false;
+        char[] s1=s.toCharArray();
+        char[] s2=t.toCharArray();
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+        int n1=s1.length;
+        int n2=s2.length;
+        if(n1!=n2) return false;
+        for(int i=0;i<n1;i++){
+            if(s1[i]!=s2[i]) return false;
         }
         return true;
     }
