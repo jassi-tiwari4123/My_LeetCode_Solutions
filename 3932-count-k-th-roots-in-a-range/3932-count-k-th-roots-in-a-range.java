d4947747c3612d6b87531c9e9bc02a97eded619a
Jassi Tiwari <jassitiwari550@gmail.com>
2026-05-17 11:09:05 +0530

    Time: 2 ms (24.05%), Space: 41.9 MB (99.61%) - LeetHub

--- 3932-count-k-th-roots-in-a-range/3932-count-k-th-roots-in-a-range.java
+++ 3932-count-k-th-roots-in-a-range/3932-count-k-th-roots-in-a-range.java
@@ -3,14 +3,14 @@ class Solution {
         if(k==1) return r-l+1;
         int cnt=0;
         for(int i=0;;i++){
-            long res=pow(i,k,r);
+            long res=pow(i,k);
             if(res>r) break;
-            if(res>=l) cnt++;
+            if(res>=l && res<=r) cnt++;
         }
         return cnt;
     }
 
-    public long pow(long base,int exp,int limit){
+    public long pow(long base,int exp){
         if(exp==0) return 1;
         if(base==0) return 0;
         if(base==1) return 1;
