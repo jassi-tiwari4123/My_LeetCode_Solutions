b419c12e10c818c7ba5577edcd4ebb6b5ec57b59
Jassi Tiwari <jassitiwari550@gmail.com>
2026-05-26 23:43:50 +0530

    Time: 1 ms (100%), Space: 43.5 MB (89.9%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
@@ -1,19 +1,39 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
-        HashSet<Character> l=new HashSet<>();
-        HashSet<Character> h=new HashSet<>();
-        int n=word.length();
+        // HashSet<Character> l=new HashSet<>();
+        // HashSet<Character> h=new HashSet<>();
+        // int n=word.length();
+        // for(char ch:word.toCharArray()){
+        //     if(Character.isLowerCase(ch)){
+        //         l.add(ch);
+        //     }
+        //     else{
+        //         h.add(ch);
+        //     }
+        // }
+        // int res=0;
+        // for(char ch:l){
+        //     if(h.contains(Character.toUpperCase(ch))){
+        //         res++;
+        //     }
+        // }
+        // return res;
+
+
+        //optimised array
+        boolean[] low=new boolean[26];
+        boolean[] up=new boolean[26];
+        int res=0;
         for(char ch:word.toCharArray()){
-            if(Character.isLowerCase(ch)){
-                l.add(ch);
+            if(ch>='a' && ch<='z'){
+                low[ch-'a']=true;
             }
             else{
-                h.add(ch);
+                up[ch-'A']=true;
             }
         }
-        int res=0;
-        for(char ch:l){
-            if(h.contains(Character.toUpperCase(ch))){
+        for(int i=0;i<26;i++){
+            if(low[i] && up[i]){
                 res++;
             }
         }
