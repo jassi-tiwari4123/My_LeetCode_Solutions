be50531e0d8872a89cdf5e4fb91d1069bca7fe2b
Jassi Tiwari <jassitiwari550@gmail.com>
2026-05-27 09:46:19 +0530

    Time: 337 ms (15.03%), Space: 237.2 MB (11.9%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -19,24 +19,23 @@ class Solution {
     //     return dp[index][prev+1]=Math.max(inc,exc);
 
 
+
         int n=nums.length;
         int[][] dp=new int[n][n+1];
         for(int i=0;i<n;i++){
             Arrays.fill(dp[i],-1);
         }
-        return fxn(0,-1,dp,nums);
+        return fxn(0,-1,nums,n,dp);
     }
-    public int fxn(int i,int prev,int[][] dp,int[] nums){
-        if(i==nums.length){
-            return 0;
-        }
-        if(nums.length==0) return 0;
-        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+    public int fxn(int ind,int prev,int[] nums,int n,int[][] dp){
+        if(n==0) return 0;
+        if(ind==n) return 0;
+        if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
         int take=0;
-        if(prev==-1 || nums[i]>nums[prev]){
-            take=1+fxn(i+1,i,dp,nums);
+        if(prev==-1 || nums[prev]<nums[ind]){
+            take=1+fxn(ind+1,ind,nums,n,dp);
         }
-        int notTake=fxn(i+1,prev,dp,nums);
-        return dp[i][prev+1]=Math.max(take,notTake);
+        int exc=fxn(ind+1,prev,nums,n,dp);
+        return dp[ind][prev+1]=Math.max(take,exc);
     }
 }
