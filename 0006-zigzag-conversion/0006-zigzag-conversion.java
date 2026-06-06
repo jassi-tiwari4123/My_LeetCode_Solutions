638f574a719a32a8b8a6ff13bb65bbde602919f6
Jassi Tiwari <jassitiwari550@gmail.com>
2026-06-06 23:49:22 +0530

    Time: 5 ms (54.09%), Space: 46.7 MB (48.98%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,22 +1,43 @@
 class Solution {
     public String convert(String s, int numRows) {
-        StringBuilder[] ans=new StringBuilder[numRows];
+        // StringBuilder[] ans=new StringBuilder[numRows];
+        // for(int i=0;i<numRows;i++){
+        //     ans[i]=new StringBuilder();
+        // }
+        // int i=0;
+        // while(i<s.length()){
+        //     for(int index=0;index<numRows && i<s.length();index++){
+        //         ans[index].append(s.charAt(i++));
+        //     }
+        //     for(int index=numRows-2;index>0 && i<s.length();index--){
+        //         ans[index].append(s.charAt(i++));
+        //     }
+        // }
+        // StringBuilder res=new StringBuilder();
+        // for(int k=0;k<numRows;k++){
+        //     res.append(ans[k]);
+        // }
+        // return res.toString();
+
+
+        StringBuilder[] res=new StringBuilder[numRows];
         for(int i=0;i<numRows;i++){
-            ans[i]=new StringBuilder();
+            res[i]=new StringBuilder();
         }
         int i=0;
-        while(i<s.length()){
-            for(int index=0;index<numRows && i<s.length();index++){
-                ans[index].append(s.charAt(i++));
+        int n=s.length();
+        while(i<n){
+            for(int id=0;id<numRows && i<n;id++){
+                res[id].append(s.charAt(i++));
             }
-            for(int index=numRows-2;index>0 && i<s.length();index--){
-                ans[index].append(s.charAt(i++));
+            for(int id=numRows-2;id>0 && i<n;id--){
+                res[id].append(s.charAt(i++));
             }
         }
-        StringBuilder res=new StringBuilder();
-        for(int k=0;k<numRows;k++){
-            res.append(ans[k]);
+        StringBuilder ans=new StringBuilder();
+        for(int j=0;j<numRows;j++){
+            ans.append(res[j]);
         }
-        return res.toString();
+        return ans.toString();
     }
 }
