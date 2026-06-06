9e602ac671b386902279790049e0ee0352b346c2
Jassi Tiwari <jassitiwari550@gmail.com>
2026-06-06 22:56:11 +0530

    Time: 2 ms (98.84%), Space: 46.5 MB (48%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
@@ -1,23 +1,41 @@
 class Solution {
     public int[] leftRightDifference(int[] nums) {
-        int n=nums.length;
-        int[] preSum=new int[n+1];
-        int[] sufSum=new int[n+1];
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                preSum[i]+=nums[j];
-            }
-        }
+        //my approach but not optimised
+        // int n=nums.length;
+        // int[] preSum=new int[n+1];
+        // int[] sufSum=new int[n+1];
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         preSum[i]+=nums[j];
+        //     }
+        // }
         
-        for(int i=n-1;i>=0;i--){
-            for(int j=i-1;j>=0;j--){
-                sufSum[i]+=nums[j];
-            }
-        }
+        // for(int i=n-1;i>=0;i--){
+        //     for(int j=i-1;j>=0;j--){
+        //         sufSum[i]+=nums[j];
+        //     }
+        // }
+        // int[] res=new int[n];
+        // for(int i=0;i<n;i++){
+        //     res[i]=Math.abs(preSum[i]-sufSum[i]);
+        // }
+        // return res;
+
+
+        //optimsed
+        int n=nums.length;
         int[] res=new int[n];
-        for(int i=0;i<n;i++){
-            res[i]=Math.abs(preSum[i]-sufSum[i]);
+        int total=0;
+        for (int num:nums){
+            total+=num;
+        }
+        int left=0;
+        for (int i=0;i<n;i++){
+            int right=total-left-nums[i];
+            res[i]=Math.abs(left-right);
+            left+=nums[i];
         }
         return res;
     }
 }
+    
