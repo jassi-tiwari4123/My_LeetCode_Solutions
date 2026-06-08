ed5754ea337f98a5bc3a78b9f18b6918b5168223
Jassi Tiwari <jassitiwari550@gmail.com>
2026-06-08 12:30:17 +0530

    Time: 0 ms (100%), Space: 44.5 MB (81.89%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,37 +1,69 @@
 class Solution {
     public void nextPermutation(int[] nums) {
+    //     int n=nums.length;
+    //     int index=-1;
+    //     for(int i=n-2;i>=0;i--){
+    //         if(nums[i]<nums[i+1]){
+    //             index=i;
+    //             break;
+    //         }
+    //     }
+    //     //if no break point found
+    //     if(index==-1){
+    //         reverse(nums,0,n-1);
+    //         return;
+    //     }
+    //     //finding the element just greater than the nums[index]
+    //     for(int i=n-1;i>index;i--){
+    //         if(nums[i]>nums[index]){
+    //             swap(nums,i,index);
+    //             break;
+    //         }
+    //     }
+    //     reverse(nums,index+1,n-1);
+    // }
+    // public void swap(int[] nums,int i,int index){
+    //     int temp=nums[i];
+    //     nums[i]=nums[index];
+    //     nums[index]=temp;
+    // }
+    // public void reverse(int[] nums,int first,int last){
+    //     while(first<last){
+    //         swap(nums,first,last);
+    //         first++;
+    //         last--;
+    //     }
+
         int n=nums.length;
-        int index=-1;
+        int ind=-1;
         for(int i=n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
-                index=i;
+                ind=i;
                 break;
             }
         }
-        //if no break point found
-        if(index==-1){
+        if(ind==-1){
             reverse(nums,0,n-1);
             return;
         }
-        //finding the element just greater than the nums[index]
-        for(int i=n-1;i>index;i--){
-            if(nums[i]>nums[index]){
-                swap(nums,i,index);
+        for(int i=n-1;i>ind;i--){
+            if(nums[i]>nums[ind]){
+                swap(nums,i,ind);
                 break;
             }
         }
-        reverse(nums,index+1,n-1);
+        reverse(nums,ind+1,n-1);
     }
-    public void swap(int[] nums,int i,int index){
+    public void swap(int[] nums,int i,int j){
         int temp=nums[i];
-        nums[i]=nums[index];
-        nums[index]=temp;
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
-    public void reverse(int[] nums,int first,int last){
-        while(first<last){
-            swap(nums,first,last);
-            first++;
-            last--;
+    public void reverse(int[] nums,int i,int j){
+        while(i<j){
+            swap(nums,i,j);
+            i++;
+            j--;
         }
     }
 }
