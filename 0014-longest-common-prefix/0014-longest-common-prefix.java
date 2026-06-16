95d69599c205effe5ae0701e5694d4132eb36ce4
Jassi Tiwari <jassitiwari550@gmail.com>
2026-06-16 19:03:57 +0530

    Time: 1 ms (62.49%), Space: 43.4 MB (17.3%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,16 +1,28 @@
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
-        int n=strs.length;
-        if(n==0 || strs==null) return "";
+        //o(n*m)
+        // int n=strs.length;
+        // if(n==0 || strs==null) return "";
+        // String first=strs[0];
+        // for(int i=0;i<first.length();i++){
+        //     char ch=first.charAt(i);
+        //     for(int j=1;j<n;j++){
+        //         if(i>=strs[j].length() || strs[j].charAt(i)!=ch){
+        //             return first.substring(0,i);
+        //         }
+        //     }
+        // }
+        // return first;
+
+
+        //O(nlog n+m)
+        Arrays.sort(strs);
         String first=strs[0];
-        for(int i=0;i<first.length();i++){
-            char ch=first.charAt(i);
-            for(int j=1;j<n;j++){
-                if(i>=strs[j].length() || strs[j].charAt(i)!=ch){
-                    return first.substring(0,i);
-                }
-            }
+        String last=strs[strs.length-1];
+        int i=0;
+        while(i<first.length() && i<last.length() && first.charAt(i)==last.charAt(i)){
+            i++;
         }
-        return first;
+        return first.substring(0,i);
     }
 }
