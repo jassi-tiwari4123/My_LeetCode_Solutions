13279f61d495dbe568a2e4784885ccf5f223f301
Jassi Tiwari <jassitiwari550@gmail.com>
2026-06-18 01:10:16 +0530

    Time: 2 ms (88.76%), Space: 47.7 MB (90.62%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,14 +1,22 @@
 class Solution {
     public boolean canJump(int[] nums) {
+        // int n=nums.length;
+        // int Maxindex=0;
+        // for(int i=0;i<n;i++){
+        //     if(i>Maxindex){
+        //         return false;
+        //     }
+        //     else{
+        //         Maxindex=Math.max(Maxindex,i+nums[i]);
+        //     }
+        // }
+        // return true;
+
         int n=nums.length;
-        int Maxindex=0;
+        int max=0;
         for(int i=0;i<n;i++){
-            if(i>Maxindex){
-                return false;
-            }
-            else{
-                Maxindex=Math.max(Maxindex,i+nums[i]);
-            }
+            if(i>max) return false;
+            max=Math.max(max,i+nums[i]);
         }
         return true;
     }
