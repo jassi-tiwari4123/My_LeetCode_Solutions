58530a5796007c16d8ad8f7f4b3fa55668eb477f
Jassi Tiwari <jassitiwari550@gmail.com>
2026-06-17 22:35:47 +0530

    Time: 0 ms (100%), Space: 42.7 MB (73.56%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,30 @@
 class Solution {
     public int rob(int[] nums) {
+        //memo
+    //     int n=nums.length;
+    //     int[] dp=new int[n+1];
+    //     Arrays.fill(dp,-1);
+    //     return fxn(n-1,nums,dp);
+    // }
+    // public int fxn(int i,int[] nums,int[] dp){
+    //     if(i<0) return 0;
+    //     if(dp[i]!=-1) return dp[i];
+    //     int pick=nums[i]+fxn(i-2,nums,dp);
+    //     int npick=fxn(i-1,nums,dp);
+    //     return dp[i]=Math.max(pick,npick);
+
+
+        //tab
         int n=nums.length;
         int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return fxn(n-1,nums,dp);
-    }
-    public int fxn(int i,int[] nums,int[] dp){
-        if(i<0) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int pick=nums[i]+fxn(i-2,nums,dp);
-        int npick=fxn(i-1,nums,dp);
-        return dp[i]=Math.max(pick,npick);
+        dp[0]=0;
+        int pick=0;
+        for(int i=1;i<=n;i++){
+            pick=nums[i-1];
+            if(i-2>=0) pick+=dp[i-2];
+            int npick=dp[i-1];
+            dp[i]=Math.max(pick,npick);
+        }
+        return dp[n];
     }
 }
