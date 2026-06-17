2d471e74dffb6d697cad95c9d0625c96b1412e00
Jassi Tiwari <jassitiwari550@gmail.com>
2026-06-17 23:14:28 +0530

    Time: 26 ms (29.11%), Space: 47.1 MB (19.6%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,22 +1,28 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
-        int[][]dp=new int[n][amount+1];
+        int[][] dp=new int[n][amount+1];
         for(int i=0;i<n;i++){
             Arrays.fill(dp[i],-1);
         }
-        int ans= fxn(0,coins,amount,dp);
-        return ans >= 1e9 ? -1 : ans;
+        int ans=fxn(n-1,coins,amount,dp);
+        return ans>=(int)1e9?-1:ans;
     }
-    public int fxn(int ind,int[]coins,int amount,int[][]dp){
-        if(ind==coins.length ||amount<0) return (int)1e9;
-        if(amount==0) return 0;
-        if(dp[ind][amount]!=-1) return dp[ind][amount];
-        int pick=0;
-        pick=1+fxn(ind,coins,amount-coins[ind],dp);
-        int notPick=fxn(ind+1,coins,amount,dp);
-        int res=Math.min(pick,notPick);
-        
-        return dp[ind][amount]=res;
+    public int fxn(int i,int[] coins,int amt,int[][] dp){
+        if(i==0 ){
+            if(amt%coins[0]==0){
+                return amt/coins[0];
+            }
+            else{
+                return (int)1e9;
+            }
+        }
+        if(dp[i][amt]!=-1) return dp[i][amt];
+        int take=(int)1e9;
+        if(coins[i]<=amt){
+            take=1+fxn(i,coins,amt-coins[i],dp);
+        }
+        int ntake=fxn(i-1,coins,amt,dp);
+        return dp[i][amt]=Math.min(take,ntake);
     }
 }
