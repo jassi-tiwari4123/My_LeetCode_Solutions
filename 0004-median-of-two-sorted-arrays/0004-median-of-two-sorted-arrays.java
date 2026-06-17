0c7e9c9f76f547aa42777d28650bb9f1ae40b9b7
Jassi Tiwari <jassitiwari550@gmail.com>
2026-06-17 11:42:49 +0530

    Time: 1 ms (100%), Space: 48.7 MB (85.51%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -23,30 +23,58 @@ class Solution {
 
 
         //optimised TC-O(log(n+m))
-        int n=nums1.length;int m=nums2.length;
-        if(n>m){
-            return findMedianSortedArrays(nums2,nums1);
-        }
-        int size=n+m;
-        int left=(n+m+1)/2;
-        int low=0;
-        int high=n;
-        while(low<=high){
-            int mid1=low+(high-low)/2;int mid2=left-mid1;
-            int l1=Integer.MIN_VALUE;int l2=Integer.MIN_VALUE;
-            int r1=Integer.MAX_VALUE;int r2=Integer.MAX_VALUE;
-            if(mid1<n) r1=nums1[mid1];if(mid2<m) r2=nums2[mid2];
-            if(mid1-1>=0) l1=nums1[mid1-1];
-            if(mid2-1>=0) l2=nums2[mid2-1];
-            if(l1<=r2 && l2<=r1){
-                if(size%2==1){
-                    return Math.max(l1,l2);
-                }
-                return (double)(Math.max(l1,l2)+Math.min(r1,r2))/2.0;
+        // int n=nums1.length;int m=nums2.length;
+        // if(n>m){
+        //     return findMedianSortedArrays(nums2,nums1);
+        // }
+        // int size=n+m;
+        // int left=(n+m+1)/2;
+        // int low=0;
+        // int high=n;
+        // while(low<=high){
+        //     int mid1=low+(high-low)/2;int mid2=left-mid1;
+        //     int l1=Integer.MIN_VALUE;int l2=Integer.MIN_VALUE;
+        //     int r1=Integer.MAX_VALUE;int r2=Integer.MAX_VALUE;
+        //     if(mid1<n) r1=nums1[mid1];if(mid2<m) r2=nums2[mid2];
+        //     if(mid1-1>=0) l1=nums1[mid1-1];
+        //     if(mid2-1>=0) l2=nums2[mid2-1];
+        //     if(l1<=r2 && l2<=r1){
+        //         if(size%2==1){
+        //             return Math.max(l1,l2);
+        //         }
+        //         return (double)(Math.max(l1,l2)+Math.min(r1,r2))/2.0;
+        //     }
+        //     else if(l1>r2) high=mid1-1;
+        //     else low=mid1+1;
+        // }
+        // return 0.0;
+
+
+        int n=nums1.length;
+        int m=nums2.length;
+        int[] merged=new int[n+m];
+        int i=0;
+        int j=0;
+        int k=0;
+        while(i<n && j<m){
+            if(nums1[i]<=nums2[j]){
+                merged[k++]=nums1[i++];
             }
-            else if(l1>r2) high=mid1-1;
-            else low=mid1+1;
+            else{
+                merged[k++]=nums2[j++];
+            }
         }
-        return 0.0;
+        while(i<n){
+            merged[k++]=nums1[i++];
+        }
+        while(j<m){
+            merged[k++]=nums2[j++];
+        }
+        if((n+m)%2==0){
+            return (merged[((n+m)/2)-1]+merged[(n+m)/2])/2.0;
+        }
+        else{
+            return merged[(n+m)/2];
+        }
     }
 }
