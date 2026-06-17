bce19bca6850acdd463c8676d24ec89fb365ec7f
Jassi Tiwari <jassitiwari550@gmail.com>
2026-06-17 21:59:11 +0530

    Time: 2 ms (100%), Space: 47.2 MB (52.27%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,28 +1,24 @@
 class Solution {
     public int minDistance(String word1, String word2) {
+        //memo
         int n=word1.length();
         int m=word2.length();
-        int[][]dp=new int[n+1][m+1];
-        for(int i=0;i<=n;i++){
+        int[][] dp=new int[n][m];
+        for(int i=0;i<n;i++){
             Arrays.fill(dp[i],-1);
         }
-        return fxn(word1,word2,n-1,m-1,dp);
+        return fxn(n-1,m-1,word1,word2,dp);
     }
-    public int fxn(String s,String t,int i,int j,int[][]dp){
+    public int fxn(int i,int j,String s,String t,int[][] dp){
         if(i<0) return j+1;
         if(j<0) return i+1;
-        if(dp[i+1][j+1]!=-1){
-            return dp[i+1][j+1];
-        }
+        if(dp[i][j]!=-1) return dp[i][j];
         if(s.charAt(i)==t.charAt(j)){
-            dp[i+1][j+1]=fxn(s,t,i-1,j-1,dp);
+            return dp[i][j]=fxn(i-1,j-1,s,t,dp);
         }
-        else{
-            int insert=fxn(s,t,i,j-1,dp);
-            int remove=fxn(s,t,i-1,j,dp);
-            int replace=fxn(s,t,i-1,j-1,dp);
-            dp[i+1][j+1]=1+Math.min(insert,Math.min(remove,replace));
-        }
-        return dp[i+1][j+1];
+        int insert=fxn(i,j-1,s,t,dp);
+        int del=fxn(i-1,j,s,t,dp);
+        int replace=fxn(i-1,j-1,s,t,dp);
+        return dp[i][j]=1+Math.min(insert,Math.min(del,replace));
     }
 }
