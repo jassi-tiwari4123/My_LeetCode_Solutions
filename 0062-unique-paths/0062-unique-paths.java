8c24bfee861a8201a2d926263e03be06f4316bf1
Jassi Tiwari <jassitiwari550@gmail.com>
2026-06-17 22:16:34 +0530

    Time: 55 ms (0.87%), Space: 42 MB (81.24%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -4,15 +4,14 @@ class Solution {
         for(int i=0;i<m;i++){
             Arrays.fill(dp[i],-1);
         }
-        int res=compute(0,0,m,n,dp);
-        return res;
+        return fxn(m-1,n-1,dp);
     }
-    public int compute(int i,int j,int m,int n,int[][] dp){
-        if(i>=m || j>=n) return 0;
-        if(i==m-1 && j==n-1) return 1;
+    public int fxn(int i,int j,int[][]dp){
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        int down=compute(i+1,j,m,n,dp);
-        int right=compute(i,j+1,m,n,dp);
-        return dp[i][j]=right+down;
+        int up=fxn(i-1,j,dp);
+        int left=fxn(i,j-1,dp);
+        return dp[i][j]=up+left;
     }
 }
