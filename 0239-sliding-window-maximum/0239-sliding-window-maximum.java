02cf30b64a2a50323a01377f4a17bb4562f8f81e
Jassi Tiwari <jassitiwari550@gmail.com>
2026-06-18 10:39:55 +0530

    Time: 33 ms (33.79%), Space: 156.2 MB (12.93%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -18,21 +18,51 @@ class Solution {
         // return res;
 
         //optimised
+        // int n=nums.length;
+        // int[] res=new int[n-k+1];
+        // Deque<Integer> dq=new ArrayDeque<>();
+        // for(int i=0;i<n;i++){
+        //     while(!dq.isEmpty() && dq.peekFirst()<=i-k){
+        //         dq.pollFirst();
+        //     }
+        //     while(!dq.isEmpty() && nums[dq.peekLast()]<nums[i]){
+        //         dq.pollLast();
+        //     }
+        //     dq.offerLast(i);
+        //     if(i>=k-1){
+        //         res[i-k+1]=nums[dq.peekFirst()];
+        //     }
+        // }
+        // return res;
+
+
+        // int n=nums.length;
+        // int[] res=new int[n-k+1];
+        // for(int i=0;i<=n-k;i++){
+        //     int max=nums[i];
+        //     for(int j=i;j<i+k;j++){
+        //         max=Math.max(max,nums[j]);
+        //     }
+        //     res[i]=max;
+        // }
+        // return res;
+
         int n=nums.length;
-        int[] res=new int[n-k+1];
-        Deque<Integer> dq=new ArrayDeque<>();
+        int[] ans=new int[n-k+1];
+        int ind=0;
+        Deque<Integer> dq=new LinkedList<>();
         for(int i=0;i<n;i++){
+            //remove element from cur window
             while(!dq.isEmpty() && dq.peekFirst()<=i-k){
                 dq.pollFirst();
             }
+            //remove smaller
             while(!dq.isEmpty() && nums[dq.peekLast()]<nums[i]){
                 dq.pollLast();
             }
             dq.offerLast(i);
-            if(i>=k-1){
-                res[i-k+1]=nums[dq.peekFirst()];
-            }
+            if(i>=k-1) ans[ind++]=nums[dq.peekFirst()];
         }
-        return res;
+        return ans;
     }
 }
