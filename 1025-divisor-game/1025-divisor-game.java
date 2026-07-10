dc41c119f81eebe83f9d3fb7090aa064b2e97b26
Jassi Tiwari <jassitiwari550@gmail.com>
2026-07-10 12:31:03 +0530

    Time: 2 ms (5.79%), Space: 42.2 MB (34.63%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,5 +1,21 @@
 class Solution {
     public boolean divisorGame(int n) {
-        return n%2==0;
+        //normal
+        // return n%2==0;
+
+        //dp
+        if(n<=1) return false;
+        boolean[] dp=new boolean[n+1];
+        Arrays.fill(dp,false);
+        dp[1]=false;
+        for(int i=2;i<=n;i++){
+            for(int j=1;j*j<i;j++){
+                if(i%j==0 && !dp[i-j]){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
     }
 }
