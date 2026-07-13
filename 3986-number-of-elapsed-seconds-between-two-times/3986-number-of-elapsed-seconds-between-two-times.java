db3451714466f3518a7abad898d85b7ff1770092
Jassi Tiwari <jassitiwari550@gmail.com>
2026-07-13 15:34:32 +0530

    Time: 3 ms (0%), Space: 44.7 MB (0%) - LeetHub

--- 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.java
+++ 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.java
@@ -2,14 +2,11 @@ class Solution {
     public int secondsBetweenTimes(String startTime, String endTime) {
         String[] arr1=startTime.split(":");
         String[] arr2=endTime.split(":");
-        int hr2=Integer.parseInt(arr2[0]);
-        int min2=Integer.parseInt(arr2[1]);
-        int sec2=Integer.parseInt(arr2[2]);
-        int secEnd=hr2*3600+min2*60+sec2;
-        int hr1=Integer.parseInt(arr1[0]);
-        int min1=Integer.parseInt(arr1[1]);
-        int sec1=Integer.parseInt(arr1[2]);
-        int secStart=hr1*3600+min1*60+sec1;
-        return secEnd-secStart;
+        int secEnd=Integer.parseInt(arr2[0])*3600+Integer.parseInt(arr2[1])*60+Integer.parseInt(arr2[2]);
+        int secSrt=Integer.parseInt(arr1[0])*3600+Integer.parseInt(arr1[1])*60+Integer.parseInt(arr1[2]);
+        if(secEnd<secSrt) {
+            secEnd+=24*3600;
+        }
+        return secEnd-secSrt;
     }
 }
