9743cd02b25adbb11972f2b3a5d068e2faae7290
Jassi Tiwari <jassitiwari550@gmail.com>
2026-07-18 00:00:10 +0530

    Time: 14 ms (88.06%), Space: 47.3 MB (88.33%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -10,28 +10,28 @@ class Solution {
                 }
             }
         }
-        if(q.isEmpty() || q.size()==n*n) return -1;
-        int[] dirRow={1,-1,0,0};
-        int[] dirCol={0,0,-1,1};
-        int dist=-1;
+        if(q.size()==n*m || q.isEmpty()) return -1;
+        int dist=0;
+        int[] dirR={0,0,-1,1};
+        int[] dirC={-1,1,0,0};
         while(!q.isEmpty()){
             int size=q.size();
-            dist++;
             for(int i=0;i<size;i++){
                 int[] cur=q.poll();
-                int row=cur[0];
-                int col=cur[1];
+                int r=cur[0];
+                int c=cur[1];
                 for(int k=0;k<4;k++){
-                    int newRow=row+dirRow[k];
-                    int newCol=col+dirCol[k];
-                    if(newRow>=0 && newRow<n && newCol>=0 && newCol<m && grid[newRow][newCol]==0){
-                        grid[newRow][newCol]=1;
-                        q.add(new int[]{newRow,newCol});
+                    int nr=r+dirR[k];
+                    int nc=c+dirC[k];
+                    if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==0){
+                        grid[nr][nc]=1;
+                        q.add(new int[]{nr,nc});
                     }
-                    
                 }
-            } 
+               
+            }
+             dist++;
         }
-        return dist;
+        return dist-1;
     }
 }
