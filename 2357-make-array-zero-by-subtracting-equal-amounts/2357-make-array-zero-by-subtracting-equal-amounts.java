41a4cc44f3649f9f80c97322ac1f664ef92a8765
Jassi Tiwari <jassitiwari550@gmail.com>
2026-07-17 15:37:41 +0530

    Time: 1 ms (94.16%), Space: 43.1 MB (56.25%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,13 +1,13 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        //using hashmap
-        // HashMap<Integer,Integer> hm=new HashMap<>();
-        // for(int x:nums){
-        //     if(x>0){
-        //         hm.put(x,hm.getOrDefault(x,0)+1);
-        //     }
-        // }
-        // return hm.size();
+        //using hashmap-->best
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int x:nums){
+            if(x>0){
+                hm.put(x,hm.getOrDefault(x,0)+1);
+            }
+        }
+        return hm.size();
 
         //another 
         // Arrays.sort(nums);
@@ -23,22 +23,22 @@ class Solution {
 
 
         //using simulation as given in ques
-        int res=0;
-        while(true){
-            int min=Integer.MAX_VALUE;
-            for(int x:nums){
-                if(x>0){
-                    min=Math.min(x,min);
-                }
-            }
-            if(min==Integer.MAX_VALUE) break;
-            for(int i=0;i<nums.length;i++){
-                if(nums[i]>0){
-                    nums[i]-=min;
-                }
-            }
-            res++;
-        }
-        return res;
+        // int res=0;
+        // while(true){
+        //     int min=Integer.MAX_VALUE;
+        //     for(int x:nums){
+        //         if(x>0){
+        //             min=Math.min(x,min);
+        //         }
+        //     }
+        //     if(min==Integer.MAX_VALUE) break;
+        //     for(int i=0;i<nums.length;i++){
+        //         if(nums[i]>0){
+        //             nums[i]-=min;
+        //         }
+        //     }
+        //     res++;
+        // }
+        // return res;
     }
 }
