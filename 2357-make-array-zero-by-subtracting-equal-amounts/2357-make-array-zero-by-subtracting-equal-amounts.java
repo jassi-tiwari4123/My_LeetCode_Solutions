ed5b6aa8f89dcf92d0376d6f301c2b6c979a339e
Jassi Tiwari <jassitiwari550@gmail.com>
2026-07-17 15:31:36 +0530

    Time: 5 ms (12.26%), Space: 43.3 MB (22.32%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,11 +1,24 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        HashMap<Integer,Integer> hm=new HashMap<>();
+        //using hashmap
+        // HashMap<Integer,Integer> hm=new HashMap<>();
+        // for(int x:nums){
+        //     if(x>0){
+        //         hm.put(x,hm.getOrDefault(x,0)+1);
+        //     }
+        // }
+        // return hm.size();
+
+        //another
+        Arrays.sort(nums);
+        int res=0;
+        int prev=-1;
         for(int x:nums){
-            if(x>0){
-                hm.put(x,hm.getOrDefault(x,0)+1);
+            if(x>0 && x!=prev){
+                res++;
+                prev=x;
             }
         }
-        return hm.size();
+        return res;
     }
 }
