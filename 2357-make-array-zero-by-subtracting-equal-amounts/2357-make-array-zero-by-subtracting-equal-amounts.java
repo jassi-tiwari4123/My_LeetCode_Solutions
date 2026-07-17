4231bb3cc89a57e90612c44fe3d43d9e2b385bc1
Jassi Tiwari <jassitiwari550@gmail.com>
2026-07-17 15:36:27 +0530

    Time: 3 ms (20.54%), Space: 42.4 MB (99.43%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -9,15 +9,35 @@ class Solution {
         // }
         // return hm.size();
 
-        //another
-        Arrays.sort(nums);
+        //another 
+        // Arrays.sort(nums);
+        // int res=0;
+        // int prev=-1;
+        // for(int x:nums){
+        //     if(x>0 && x!=prev){
+        //         res++;
+        //         prev=x;
+        //     }
+        // }
+        // return res;
+
+
+        //using simulation as given in ques
         int res=0;
-        int prev=-1;
-        for(int x:nums){
-            if(x>0 && x!=prev){
-                res++;
-                prev=x;
+        while(true){
+            int min=Integer.MAX_VALUE;
+            for(int x:nums){
+                if(x>0){
+                    min=Math.min(x,min);
+                }
             }
+            if(min==Integer.MAX_VALUE) break;
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]>0){
+                    nums[i]-=min;
+                }
+            }
+            res++;
         }
         return res;
     }
