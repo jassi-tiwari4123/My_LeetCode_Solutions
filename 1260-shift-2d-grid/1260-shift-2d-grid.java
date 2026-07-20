ee2237701a6c22dcbb081bd298e769f76af95afc
Jassi Tiwari <jassitiwari550@gmail.com>
2026-07-20 14:27:10 +0530

    Time: 3 ms (98.39%), Space: 47 MB (79.54%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int n=grid.length;
+        int m=grid[0].length;
+        int totalLen=n*m;
+        int shift=k%totalLen;
+        List<List<Integer>> res=new ArrayList<>();
+        int z=0;
+        int[] samp=new int[totalLen];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                samp[z++]=grid[i][j];
+            }
+        }
+        int x=0;
+        int[] inter=new int[totalLen];
+        for(int i=totalLen-shift;i<totalLen;i++){
+            inter[x]=samp[i];
+            x++;
+        }
+        for(int i=0;i<totalLen-shift;i++){
+            inter[x]=samp[i];
+            x++;
+        }
+        int ind=0;
+        for(int i=0;i<n;i++){
+            List<Integer> part=new ArrayList<>();
+            for(int j=0;j<m;j++){
+                part.add(inter[ind++]);
+            }
+            res.add(part);
+        }   
+        return res;
+    }
+}
