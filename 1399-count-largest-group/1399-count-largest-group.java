2756f7a63dd7ad5ad32f1c8262d6b66c3d7b9f36
Jassi Tiwari <jassitiwari550@gmail.com>
2026-07-22 14:52:33 +0530

    Time: 15 ms (15.28%), Space: 45.4 MB (8.11%) - LeetHub

--- 1399-count-largest-group/1399-count-largest-group.java
+++ 1399-count-largest-group/1399-count-largest-group.java
@@ -1,17 +1,20 @@
 class Solution {
     public int countLargestGroup(int n) {
-        HashMap<Integer,Integer> hm=new HashMap<>();
+        HashMap<Integer,ArrayList<Integer>> hm=new HashMap<>();
         for(int i=1;i<=n;i++){
             int sum=sumOfDig(i);
-            hm.put(sum,hm.getOrDefault(sum,0)+1);
+            if(!hm.containsKey(sum)){
+                hm.put(sum,new ArrayList<>());
+            }
+            hm.get(sum).add(i);
         }
         int max=0;
-        for(int count:hm.values()){
-            max=Math.max(max,count);
+        for(ArrayList<Integer> a:hm.values()){
+            max=Math.max(max,a.size());
         }
         int res=0;
-        for(int count:hm.values()){
-            if(count==max){
+        for(ArrayList<Integer> a:hm.values()){
+            if(max==a.size()){
                 res++;
             }
         }
@@ -20,10 +23,42 @@ class Solution {
     public int sumOfDig(int x){
         int sum=0;
         while(x>0){
-            int dig=x%10;
-            sum+=dig;
+            sum+=x%10;
             x/=10;
         }
         return sum;
     }
 }
+
+
+
+
+// class Solution {
+//     public int countLargestGroup(int n) {
+//         HashMap<Integer,Integer> hm=new HashMap<>();
+//         for(int i=1;i<=n;i++){
+//             int sum=sumOfDig(i);
+//             hm.put(sum,hm.getOrDefault(sum,0)+1);
+//         }
+//         int max=0;
+//         for(int count:hm.values()){
+//             max=Math.max(max,count);
+//         }
+//         int res=0;
+//         for(int count:hm.values()){
+//             if(count==max){
+//                 res++;
+//             }
+//         }
+//         return res;
+//     }
+//     public int sumOfDig(int x){
+//         int sum=0;
+//         while(x>0){
+//             int dig=x%10;
+//             sum+=dig;
+//             x/=10;
+//         }
+//         return sum;
+//     }
+// }
