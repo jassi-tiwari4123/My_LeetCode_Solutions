10a93967cd030edcc7738f53a03fb4bc0c6527bf
Jassi Tiwari <jassitiwari550@gmail.com>
2026-07-27 12:49:41 +0530

    Time: 5 ms (83.21%), Space: 44.1 MB (71.72%) - LeetHub

---
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        int n=arr.length;
+        Arrays.sort(arr);
+        if(n==2) return true;
+        int d=arr[1]-arr[0];
+        for(int i=2;i<n;i++){
+            if(arr[i]-arr[i-1]!=d) return false;
+        }
+        return true;
+    }
+}
