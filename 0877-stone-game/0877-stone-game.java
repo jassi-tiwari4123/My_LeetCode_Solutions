29f602248b1377fcf7046f31c82cc9c011da8b04
Jassi Tiwari <jassitiwari550@gmail.com>
2026-08-02 09:37:28 +0530

    Time: 15 ms (5.73%), Space: 56.1 MB (5.29%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,18 +1,15 @@
 class Solution {
-    int[][] dp;
+    Integer[][] dp;
     public boolean stoneGame(int[] piles) {
         int n=piles.length;
-        dp=new int[501][501];
-        for(int i=0;i<=500;i++){
-            Arrays.fill(dp[i],Integer.MIN_VALUE);
-        }
+        dp=new Integer[501][501];
         int x=fxn(0,n-1,piles);
         if(x>=0) return true;
         return false;
     }
     public int fxn(int i,int j,int[] piles){
         if(i==j) return piles[i];
-        if(dp[i][j]!=Integer.MIN_VALUE) return dp[i][j];
+        if(dp[i][j]!=null) return dp[i][j];
         int left=piles[i]-fxn(i+1,j,piles);
         int right=piles[j]-fxn(i,j-1,piles);
         dp[i][j]=Math.max(left,right);
