0bedff641680dbd0e2693f158552a62a71f3857e
Jassi Tiwari <jassitiwari550@gmail.com>
2026-08-05 16:02:37 +0530

    Time: 43 ms (17.42%), Space: 130.2 MB (17.16%) - LeetHub

--- 3693-climbing-stairs-ii/3693-climbing-stairs-ii.java
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.java
@@ -1,24 +1,116 @@
 class Solution {
+    int[] dp;
     public int climbStairs(int n, int[] costs) {
-        int[] dp=new int[n+1];
+        dp=new int[n+1];
         Arrays.fill(dp,-1);
-        return compute(0,n,costs,dp);
+        return fxn(n,costs);
     }
-    public int compute(int index,int n,int[] costs,int[] dp){
-        if(index==n){
-            return 0;
+    public int fxn(int ind,int[] cost){
+        if(ind==0 || ind<0) return 0;
+        if(dp[ind]!=-1) return dp[ind];
+        int ans=Integer.MAX_VALUE;
+        if(ind>=0){
+            ans=Math.min(ans,cost[ind-1]+1+fxn(ind-1,cost));
         }
-        if(dp[index]!=-1) return dp[index];
-        int ans=Integer.MAX_VALUE/2;
-        if(index+1<=n){
-            ans=Math.min(ans,compute(index+1,n,costs,dp)+costs[index]+1*1);
+        if(ind>1){
+            ans=Math.min(ans,cost[ind-1]+4+fxn(ind-2,cost));
         }
-        if(index+2<=n){
-            ans=Math.min(ans,compute(index+2,n,costs,dp)+costs[index+1]+2*2);
+        if(ind>2){
+            ans=Math.min(ans,cost[ind-1]+9+fxn(ind-3,cost));
         }
-        if(index+3<=n){
-            ans=Math.min(ans,compute(index+3,n,costs,dp)+costs[index+2]+3*3);
-        }
-        return dp[index]=ans; 
+        return dp[ind]=ans;
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int climbStairs(int n, int[] costs) {
+//         int[] dp=new int[n+1];
+//         Arrays.fill(dp,-1);
+//         return compute(0,n,costs,dp);
+//     }
+//     public int compute(int index,int n,int[] costs,int[] dp){
+//         if(index==n){
+//             return 0;
+//         }
+//         if(dp[index]!=-1) return dp[index];
+//         int ans=Integer.MAX_VALUE/2;
+//         if(index+1<=n){
+//             ans=Math.min(ans,compute(index+1,n,costs,dp)+costs[index]+1*1);
+//         }
+//         if(index+2<=n){
+//             ans=Math.min(ans,compute(index+2,n,costs,dp)+costs[index+1]+2*2);
+//         }
+//         if(index+3<=n){
+//             ans=Math.min(ans,compute(index+3,n,costs,dp)+costs[index+2]+3*3);
+//         }
+//         return dp[index]=ans; 
+//     }
+// }
