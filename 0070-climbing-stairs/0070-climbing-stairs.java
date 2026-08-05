2b671caa31a2ddb7da3fe20723cc82002f745cd4
Jassi Tiwari <jassitiwari550@gmail.com>
2026-08-05 14:51:26 +0530

    Time: 0 ms (100%), Space: 42.2 MB (17.69%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,23 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp=new int[n+1];
-        dp[0]=1;
-        dp[1]=1;
+        // int[] dp=new int[n+1];
+        // dp[0]=1;
+        // dp[1]=1;
+        // for(int i=2;i<=n;i++){
+        //     dp[i]=dp[i-1]+dp[i-2];
+        // }
+        // return dp[n];
+
+
+        //space optimsation
+        int prev=1;
+        int prev1=1;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            int cur=prev+prev1;
+            prev1=prev;
+            prev=cur;
         }
-        return dp[n];
+        return prev;
     }
 }
 
