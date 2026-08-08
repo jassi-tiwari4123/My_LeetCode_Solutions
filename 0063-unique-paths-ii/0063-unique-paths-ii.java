9fdaa05ae1b2ffd626595a75a2173be6eb5fb8d5
Jassi Tiwari <jassitiwari550@gmail.com>
2026-08-08 15:05:43 +0530

    Time: 1 ms (17.12%), Space: 43.2 MB (92.05%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -30,25 +30,51 @@
 
 
 //memo
+// class Solution {
+//     int[][] dp;
+//     public int uniquePathsWithObstacles(int[][] grid) {
+//         int n=grid.length;
+//         int m=grid[0].length;
+//         dp=new int[n][m];
+//         for(int i=0;i<n;i++){
+//             Arrays.fill(dp[i],-1);
+//         }
+//         if(grid[0][0]==1 || grid[n-1][m-1]==1) return 0;
+//         return fxn(n-1,m-1,grid);
+//     }
+//     public int fxn(int i,int j,int[][] grid){
+//         if(i==0 && j==0) return 1;
+//         if(i<0 || j<0) return 0;
+//         if(grid[i][j]==1) return 0;
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         int top=fxn(i-1,j,grid);
+//         int left=fxn(i,j-1,grid);
+//         return dp[i][j]=top+left;
+//     }
+// }
+
+
 class Solution {
     int[][] dp;
     public int uniquePathsWithObstacles(int[][] grid) {
+        // code here
         int n=grid.length;
         int m=grid[0].length;
         dp=new int[n][m];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
-        }
         if(grid[0][0]==1 || grid[n-1][m-1]==1) return 0;
-        return fxn(n-1,m-1,grid);
+        dp[0][0]=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0) continue;
+                if(i-1>=0){
+                    if(grid[i][j]!=1) dp[i][j]=dp[i-1][j];
+                }
+                if(j-1>=0){
+                    if(grid[i][j]!=1) dp[i][j]+=dp[i][j-1];
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
-    public int fxn(int i,int j,int[][] grid){
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0) return 0;
-        if(grid[i][j]==1) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int top=fxn(i-1,j,grid);
-        int left=fxn(i,j-1,grid);
-        return dp[i][j]=top+left;
-    }
-}
+    
+};
