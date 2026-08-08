93832e1a9ff3965bd2c525aea8398f10dce1b090
Jassi Tiwari <jassitiwari550@gmail.com>
2026-08-08 11:50:54 +0530

    Time: 1 ms (14.56%), Space: 41.9 MB (92.61%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -46,22 +46,44 @@
 
 
 
-//rec
+//rec+memo
+// class Solution {
+//     int[][] dp;
+//     public int uniquePaths(int m, int n) {
+//         dp=new int[m][n];
+//         for(int i=0;i<m;i++){
+//             Arrays.fill(dp[i],-1);
+//         }
+//         return fxn(m-1,n-1);
+//     }
+//     public int fxn(int i,int j){
+//         if(i==0 && j==0) return 1;
+//         if(i<0 || j<0) return 0;
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         int top=fxn(i-1,j);
+//         int left=fxn(i,j-1);
+//         return dp[i][j]=left+top;
+//     }
+// }
+
+
+//tab
 class Solution {
     int[][] dp;
     public int uniquePaths(int m, int n) {
         dp=new int[m][n];
+        dp[0][0]=1;
         for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) continue;
+                if(i-1>=0){
+                    dp[i][j]=dp[i-1][j];
+                }
+                if(j-1>=0){
+                    dp[i][j]+=dp[i][j-1];
+                }
+            }
         }
-        return fxn(m-1,n-1);
-    }
-    public int fxn(int i,int j){
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int top=fxn(i-1,j);
-        int left=fxn(i,j-1);
-        return dp[i][j]=left+top;
+        return dp[m-1][n-1];
     }
 }
