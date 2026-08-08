eeb39c72d0c5d0606df030b5c77714a74912d0e6
Jassi Tiwari <jassitiwari550@gmail.com>
2026-08-08 11:45:08 +0530

    Time: 0 ms (100%), Space: 42.3 MB (13.16%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,37 +1,67 @@
-class Solution {
-    public int uniquePaths(int m, int n) {
-        //memo
-    //     int[][] dp=new int[m][n];
-    //     for(int i=0;i<m;i++){
-    //         Arrays.fill(dp[i],-1);
-    //     }
-    //     return fxn(m-1,n-1,dp);
-    // }
-    // public int fxn(int i,int j,int[][]dp){
-    //     if(i==0 && j==0) return 1;
-    //     if(i<0 || j<0) return 0;
-    //     if(dp[i][j]!=-1) return dp[i][j];
-    //     int up=fxn(i-1,j,dp);
-    //     int left=fxn(i,j-1,dp);
-    //     return dp[i][j]=up+left;
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+//         //memo
+//     //     int[][] dp=new int[m][n];
+//     //     for(int i=0;i<m;i++){
+//     //         Arrays.fill(dp[i],-1);
+//     //     }
+//     //     return fxn(m-1,n-1,dp);
+//     // }
+//     // public int fxn(int i,int j,int[][]dp){
+//     //     if(i==0 && j==0) return 1;
+//     //     if(i<0 || j<0) return 0;
+//     //     if(dp[i][j]!=-1) return dp[i][j];
+//     //     int up=fxn(i-1,j,dp);
+//     //     int left=fxn(i,j-1,dp);
+//     //     return dp[i][j]=up+left;
 
-        //tab
-        int[][] dp=new int[m][n];
-        dp[0][0]=1;
+//         //tab
+//         int[][] dp=new int[m][n];
+//         dp[0][0]=1;
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(i==0 && j==0) continue;
+//                 int up=0;
+//                 int left=0;
+//                 if(i>0){
+//                     up=dp[i-1][j];
+//                 }
+//                 if(j>0){
+//                     left=dp[i][j-1];
+//                 }
+//                 dp[i][j]=left+up;
+//             }
+//         }
+//         return dp[m-1][n-1];
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+//rec
+class Solution {
+    int[][] dp;
+    public int uniquePaths(int m, int n) {
+        dp=new int[m][n];
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0) continue;
-                int up=0;
-                int left=0;
-                if(i>0){
-                    up=dp[i-1][j];
-                }
-                if(j>0){
-                    left=dp[i][j-1];
-                }
-                dp[i][j]=left+up;
-            }
+            Arrays.fill(dp[i],-1);
         }
-        return dp[m-1][n-1];
+        return fxn(m-1,n-1);
+    }
+    public int fxn(int i,int j){
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int top=fxn(i-1,j);
+        int left=fxn(i,j-1);
+        return dp[i][j]=left+top;
     }
 }
