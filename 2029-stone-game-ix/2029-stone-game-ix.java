952473b0a6dd67f8523f29ed05158853452c09af
Jassi Tiwari <jassitiwari550@gmail.com>
2026-08-16 15:19:41 +0530

    Time: 7 ms (29.05%), Space: 118.5 MB (8.78%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int c0=0;
+        int c1=0;
+        int c2=0;
+        for(int x:stones){
+            int v=x%3;
+            if(v==0){
+                c0++;
+            } 
+            else if(v==1){
+                c1++;
+            } 
+            else c2++;
+        }
+        if(c0%2==0){
+            return c1>=1 && c2>=1;
+        }
+        return Math.abs(c1-c2)>2;
+    }
+}
